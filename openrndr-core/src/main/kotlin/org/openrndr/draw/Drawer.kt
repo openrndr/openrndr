3be8e1a7fe9a0e45375e904608b28ae7f4c96c2b
Edwin Jakobs <edwin@rndr.studio>
2018-02-20 21:53:37 +0100

    Added lineSegment(x0,y0,x1,y1)

--- openrndr-core/src/main/kotlin/org/openrndr/draw/Drawer.kt
+++ openrndr-core/src/main/kotlin/org/openrndr/draw/Drawer.kt
@@ -735,6 +735,10 @@ class Drawer(val driver: Driver) {
     }
 
 
+    fun lineSegment(x0:Double, y0:Double, x1:Double, y1:Double) {
+        lineSegment(Vector2(x0, y0), Vector2(x1, y1))
+    }
+
     fun lineSegment(lineSegment: LineSegment) {
         lineSegment(lineSegment.start, lineSegment.end)
     }
