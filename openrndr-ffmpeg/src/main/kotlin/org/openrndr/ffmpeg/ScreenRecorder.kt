22cbf00d21d3fb988bef8d3b706c7d78aea91b69
edwin <edwin@lustlab.net>
2018-04-11 10:48:41 +0200

    Changed behaviour of ScreenRecorder - resets view and model matrices - determines filename with trailing zeroes - clears the background when Program.background is set

--- openrndr-ffmpeg/src/main/kotlin/org/openrndr/ffmpeg/ScreenRecorder.kt
+++ openrndr-ffmpeg/src/main/kotlin/org/openrndr/ffmpeg/ScreenRecorder.kt
@@ -16,16 +16,29 @@ class ScreenRecorder:Extension {
     lateinit var videoWriter: VideoWriter
     lateinit var frame: RenderTarget
     override fun setup(program: Program) {
+
+        fun Int.z(zeroes:Int=2):String {
+            val sv = this.toString()
+            var prefix = ""
+            for (i in 0 until Math.max(zeroes-sv.length, 0)) {
+                prefix += "0"
+            }
+            return "$prefix$sv"
+        }
+
         frame = renderTarget(program.width, program.height) {
             colorBuffer()
             depthBuffer()
         }
         val dt = LocalDateTime.now()
-        videoWriter = VideoWriter().output("${program.javaClass.simpleName}-${dt.year}-${dt.month.value}-${dt.dayOfMonth}-${dt.hour}-${dt.minute}-${dt.second}.mp4").size(program.width, program.height).frameRate(30).start()
+        videoWriter = VideoWriter().output("${program.javaClass.simpleName}-${dt.year.z(4)}-${dt.month.value.z()}-${dt.dayOfMonth.z()}-${dt.hour.z()}.${dt.minute.z()}.${dt.second.z()}.mp4").size(program.width, program.height).frameRate(30).start()
     }
 
     override fun beforeDraw(drawer: Drawer, program: Program) {
         frame.bind()
+        program.backgroundColor?.let {
+            drawer.background(it)
+        }
     }
 
     override fun afterDraw(drawer: Drawer, program: Program) {
@@ -33,6 +46,7 @@ class ScreenRecorder:Extension {
         videoWriter.frame(frame.colorBuffer(0))
         drawer.isolated {
             drawer.ortho()
+            drawer.model = Matrix44.IDENTITY
             drawer.view = Matrix44.IDENTITY
             drawer.image(frame.colorBuffer(0))
         }
