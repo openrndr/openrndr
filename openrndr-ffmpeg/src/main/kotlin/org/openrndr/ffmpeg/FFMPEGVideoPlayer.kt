4d2cb6192345b128d3c51242b0aa32e743a028f8
edwin <edwin@lustlab.net>
2018-07-02 15:00:44 +0200

    Added preliminary support for webcams

--- openrndr-ffmpeg/src/main/kotlin/org/openrndr/ffmpeg/FFMPEGVideoPlayer.kt
+++ openrndr-ffmpeg/src/main/kotlin/org/openrndr/ffmpeg/FFMPEGVideoPlayer.kt
@@ -8,28 +8,52 @@ import org.openrndr.internal.gl3.ColorBufferGL3
 import java.io.File
 import java.nio.ByteBuffer
 
-class FFMPEGVideoPlayer(url:String) {
-
+class FFMPEGVideoPlayer private constructor(url: String) {
     companion object {
-        fun fromURL(url:String):FFMPEGVideoPlayer {
+        fun fromURL(url: String): FFMPEGVideoPlayer {
             return FFMPEGVideoPlayer(url)
         }
-        fun fromFile(filename:String):FFMPEGVideoPlayer {
+
+        fun fromFile(filename: String): FFMPEGVideoPlayer {
             return FFMPEGVideoPlayer(File(filename).toURI().toURL().toExternalForm())
+        }
+
+        fun listDevices():List<String> {
+            return FFmpegFrameGrabber.getDeviceDescriptions().toList()
+        }
+
+        fun fromDevice(deviceName: String): FFMPEGVideoPlayer {
+            val osName = System.getProperty("os.name").toLowerCase()
+            val format: String
+            format = when {
+                "windows" in osName -> {
+                    "dshow"
+                }
+                "mac os x" in osName -> {
+                    "avfoundation"
+                }
+                "linux" in osName -> {
+                    "video4linux"
+                }
+                else -> throw RuntimeException("unsupported os: $osName")
+            }
+
+            val player = FFMPEGVideoPlayer(deviceName)
+            player.frameGrabber.format = format
+            return player
         }
     }
 
     internal var frameGrabber = FFmpegFrameGrabber(url)
-
-    var colorBuffer:ColorBuffer? = null
+    private var colorBuffer: ColorBuffer? = null
 
     fun start() {
         frameGrabber.start()
     }
 
+
     fun next() {
         val frame = frameGrabber.grabImage()
-
         if (frame != null) {
             if (colorBuffer == null && frame.imageWidth > 0 && frame.imageHeight > 0) {
                 colorBuffer = ColorBuffer.create(frame.imageWidth, frame.imageHeight, format = ColorFormat.RGB).apply {
