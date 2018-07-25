e386b9d1346db1eb44df6e96bab3e5aa8fe6e0a5
edwin <edwin@lustlab.net>
2018-07-25 13:02:24 +0200

    Improved error handling for ColorBufferLoader

--- openrndr-core/src/main/kotlin/org/openrndr/draw/ColorBufferProxy.kt
+++ openrndr-core/src/main/kotlin/org/openrndr/draw/ColorBufferProxy.kt
@@ -1,15 +1,19 @@
 package org.openrndr.draw
 
+import mu.KotlinLogging
 import org.openrndr.events.Event
 import org.openrndr.internal.ColorBufferLoader
 
+private val logger = KotlinLogging.logger {}
 
-class ColorBufferProxy(val url: String, val loader: ColorBufferLoader) {
+data class ColorBufferProxy(val url: String, val loader: ColorBufferLoader, val persistent: Boolean) {
     class ProxyEvent
 
     class Events {
         val loaded = Event<ProxyEvent>()
         val unloaded = Event<ProxyEvent>()
+        val retry = Event<ProxyEvent>()
+        val error = Event<ProxyEvent>()
     }
 
     val events = Events()
@@ -17,17 +21,20 @@ class ColorBufferProxy(val url: String, val loader: ColorBufferLoader) {
     enum class State {
         NOT_LOADED,
         QUEUED,
-        LOADED
+        LOADED,
+        RETRY,
+        ERROR
     }
 
-    internal var state = State.NOT_LOADED
+    internal var realState = State.NOT_LOADED
     internal var realColorBuffer: ColorBuffer? = null
 
-    var persistent = false
+    val state get() = realState
+
     val colorBuffer: ColorBuffer?
         get() {
             lastTouched = System.currentTimeMillis()
-            if (state == State.NOT_LOADED) {
+            if (realState == State.NOT_LOADED) {
                 queue()
             }
             return realColorBuffer
@@ -37,13 +44,26 @@ class ColorBufferProxy(val url: String, val loader: ColorBufferLoader) {
 
     fun queue() {
         touch()
-        if (state == State.NOT_LOADED) {
+        if (realState == State.NOT_LOADED) {
             loader.queue(this)
-            state = State.QUEUED
+            realState = State.QUEUED
         }
     }
 
     fun touch() {
         lastTouched = System.currentTimeMillis()
+    }
+
+    fun retry() {
+        if (realState == State.RETRY) {
+            logger.debug {
+                "retry requested"
+            }
+            realState = State.NOT_LOADED
+        } else {
+            logger.warn {
+                "proxy is not in retry"
+            }
+        }
     }
 }
--- openrndr-core/src/main/kotlin/org/openrndr/internal/ColorBufferLoader.kt
+++ openrndr-core/src/main/kotlin/org/openrndr/internal/ColorBufferLoader.kt
@@ -1,8 +1,12 @@
 package org.openrndr.internal
 
+import mu.KotlinLogging
 import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.ColorBuffer
 import org.openrndr.draw.ColorBufferProxy
+import java.io.IOException
+
+private val logger = KotlinLogging.logger {}
 
 val colorBufferLoader by lazy {
     ColorBufferLoader.create()
@@ -12,15 +16,14 @@ class ColorBufferLoader {
     val loadQueue = mutableListOf<ColorBufferProxy>()
     val unloadQueue = mutableListOf<ColorBufferProxy>()
 
-
     fun queue(colorBufferProxy: ColorBufferProxy) {
         synchronized(loadQueue) { loadQueue.add(colorBufferProxy) }
     }
 
-    fun loadFromUrl(url: String): ColorBufferProxy {
-        val proxy = ColorBufferProxy(url, this).apply {
+    fun loadFromUrl(url: String, persistent:Boolean = false): ColorBufferProxy {
+        val proxy = ColorBufferProxy(url, this, persistent).apply {
             lastTouched = System.currentTimeMillis()
-            state = ColorBufferProxy.State.QUEUED
+            realState = ColorBufferProxy.State.QUEUED
         }
         synchronized(loadQueue) {
             loadQueue.add(proxy)
@@ -31,36 +34,49 @@ class ColorBufferLoader {
     companion object {
         fun create(): ColorBufferLoader {
             val loader = ColorBufferLoader()
-            val rf = Driver.instance.createResourceThread {
+            Driver.instance.createResourceThread {
+                logger.debug { "ColorBufferLoader thread started" }
                 while (true) {
-
                     if (!loader.loadQueue.isEmpty()) {
                         val proxy = synchronized(loader.loadQueue) {
-                            loader.loadQueue.sortBy { it.lastTouched }
+                            //loader.loadQueue.sortBy { it.lastTouched }
                             loader.loadQueue.removeAt(0)
                         }
-                        val cb = ColorBuffer.fromUrl(proxy.url)
-                        proxy.realColorBuffer = cb
-                        proxy.state = ColorBufferProxy.State.LOADED
-                        if (!proxy.persistent) {
-                            synchronized(loader.unloadQueue) {
-                                loader.unloadQueue.add(proxy)
+                        try {
+                            val cb = ColorBuffer.fromUrl(proxy.url)
+                            proxy.realColorBuffer = cb
+                            proxy.realState = ColorBufferProxy.State.LOADED
+                            if (!proxy.persistent) {
+                                synchronized(loader.unloadQueue) {
+                                    loader.unloadQueue.add(proxy)
+                                }
                             }
+                            logger.debug { "successfully loaded $proxy" }
+                            proxy.events.loaded.trigger(ColorBufferProxy.ProxyEvent())
+                        } catch (e: IOException) {
+                            logger.error { e.stackTrace }
+                            proxy.realState = ColorBufferProxy.State.RETRY
+                            proxy.events.retry.trigger(ColorBufferProxy.ProxyEvent())
+                        } catch (e: Exception) {
+                            logger.error { "unexpected exception while loading $proxy" }
+                            logger.error { e.stackTrace }
+                            proxy.realState = ColorBufferProxy.State.ERROR
+                            proxy.events.error.trigger(ColorBufferProxy.ProxyEvent())
+                            // maybe come to a grinding halt here?
                         }
-                        proxy.events.loaded.trigger(ColorBufferProxy.ProxyEvent())
                     }
-
 
                     synchronized(loader.unloadQueue) {
                         if (!loader.unloadQueue.isEmpty()) {
-                            loader.unloadQueue.sortBy { it.lastTouched }
+                            //loader.unloadQueue.sortBy { it.lastTouched }
                             val dt = System.currentTimeMillis() - loader.unloadQueue[0].lastTouched
                             if (dt > 5000) {
                                 val proxy = loader.unloadQueue.removeAt(0)
                                 proxy.realColorBuffer?.destroy()
                                 proxy.realColorBuffer = null
-                                proxy.state = ColorBufferProxy.State.NOT_LOADED
+                                proxy.realState = ColorBufferProxy.State.NOT_LOADED
                                 proxy.events.unloaded.trigger(ColorBufferProxy.ProxyEvent())
+                                logger.debug { "unloaded $proxy" }
                             }
                         }
                     }
