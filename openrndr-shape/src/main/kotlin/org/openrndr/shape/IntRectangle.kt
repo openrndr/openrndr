adb2ac66de80b79b1d0c5829bb37e3fef0ca7998
edwin <edwin@lustlab.net>
2018-07-25 10:55:46 +0200

    Added IntRectangle.rectangle

--- openrndr-shape/src/main/kotlin/org/openrndr/shape/IntRectangle.kt
+++ openrndr-shape/src/main/kotlin/org/openrndr/shape/IntRectangle.kt
@@ -2,14 +2,13 @@ package org.openrndr.shape
 
 import org.openrndr.math.IntVector2
 
+@Suppress("MemberVisibilityCanBePrivate", "unused")
 class IntRectangle(val corner: IntVector2, val width: Int, val height: Int) {
-
     constructor(x: Int, y: Int, width: Int, height: Int) : this(IntVector2(x, y), width, height)
 
     val x get() = corner.x
     val y get() = corner.y
-
     val center get() = corner + IntVector2(width / 2, height / 2)
-
     val area get() = width * height
+    val rectangle get() = Rectangle(corner.vector2, width.toDouble(), height.toDouble())
 }
