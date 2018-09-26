c173cc13833838ac4e8a426c60eefb123b477d7b
Edwin Jakobs <edwin@rndr.studio>
2018-09-26 22:07:46 +0200

    Improved Rectangle API - added scaled() - added widthScaledTo() - added heightScaledTo() - added moved()

--- openrndr-shape/src/main/kotlin/org/openrndr/shape/Rectangle.kt
+++ openrndr-shape/src/main/kotlin/org/openrndr/shape/Rectangle.kt
@@ -1,3 +1,5 @@
+@file:Suppress("unused")
+
 package org.openrndr.shape
 
 import org.openrndr.math.Vector2
@@ -6,11 +8,15 @@ data class Rectangle(val corner: Vector2, val width: Double, val height: Double)
 
     constructor(x: Double, y: Double, width: Double, height: Double) : this(Vector2(x, y), width, height)
 
+    /** the center of the rectangle */
     val center: Vector2
-        get() = corner + Vector2(width / 2.0, height / 2)
+        get() = corner + Vector2(width / 2, height / 2)
+
+    /** the unitless area covered by this rectangle*/
     val area: Double
         get() = width * height
 
+    /** returns a position for parameterized coordinates [u] and [v] between 0 and 1 */
     fun position(u: Double, v: Double): Vector2 {
         return corner + Vector2(u * width, v * height)
     }
@@ -18,18 +24,42 @@ data class Rectangle(val corner: Vector2, val width: Double, val height: Double)
     val x: Double get() = corner.x
     val y: Double get() = corner.y
 
-    val shape: Shape
-        get() {
-            return Shape(listOf(contour))
-        }
+    /** [Shape] representation of the rectangle */
+    val shape get() = Shape(listOf(contour))
 
-    val contour: ShapeContour
-        get() {
-            return ShapeContour.fromPoints(listOf(corner, corner + Vector2(width, 0.0), corner + Vector2(width, height), corner + Vector2(0.0, height), corner), true)
-        }
+    /** [ShapeContour] representation of the rectangle */
+    val contour
+        get() =
+            ShapeContour.fromPoints(listOf(corner, corner + Vector2(width, 0.0),
+                    corner + Vector2(width, height),
+                    corner + Vector2(0.0, height),
+                    corner), true)
 
+    /** create a new [Rectangle] instance with offset edges */
     fun offsetEdges(offset: Double, offsetY: Double = offset): Rectangle {
         return Rectangle(Vector2(corner.x - offset, corner.y - offsetY), width + 2 * offset, height + 2 * offsetY)
+    }
+
+    /** creates a new [Rectangle] with dimensions scaled by [scale] and [scaleY] */
+    fun scaled(scale: Double, scaleY: Double = scale): Rectangle {
+        return Rectangle(corner, width * scale, height * scaleY)
+    }
+
+    /** creates a new [Rectangle] with width set to [fitWidth] and height scaled proportionally */
+    fun widthScaledTo(fitWidth: Double): Rectangle {
+        val scale = fitWidth / width
+        return Rectangle(corner, fitWidth, height * scale)
+    }
+
+    /** creates a new [Rectangle] with height set to [fitHeight] and width scaled proportionally */
+    fun heightScaledTo(fitHeight: Double): Rectangle {
+        val scale = fitHeight / height
+        return Rectangle(corner, width * scale, fitHeight)
+    }
+
+    /** creates a new [Rectangle] with its position offset by [offset] */
+    fun moved(offset: Vector2): Rectangle {
+        return Rectangle(corner + offset, width, height)
     }
 
     operator fun contains(point: Vector2): Boolean {
@@ -39,8 +69,14 @@ data class Rectangle(val corner: Vector2, val width: Double, val height: Double)
                 point.y < corner.y + height)
     }
 
+    companion object {
+        /** creates a [Rectangle] around [center] with dimensions [width] and [height] */
+        fun fromCenter(center: Vector2, width: Double, height: Double) =
+                Rectangle(center.x - width / 2.0, center.y - height / 2.0, width, height)
+    }
 }
 
+/** calculates [Rectangle]-bounds for a list of [Vector2] instances */
 fun vector2Bounds(points: List<Vector2>): Rectangle {
     var minX = Double.POSITIVE_INFINITY
     var minY = Double.POSITIVE_INFINITY
@@ -56,6 +92,7 @@ fun vector2Bounds(points: List<Vector2>): Rectangle {
     return Rectangle(Vector2(minX, minY), maxX - minX, maxY - minY)
 }
 
+/** calculates [Rectangle]-bounds for a list of [Rectangle] instances */
 fun rectangleBounds(rectangles: List<Rectangle>): Rectangle {
     var minX = Double.POSITIVE_INFINITY
     var minY = Double.POSITIVE_INFINITY
@@ -69,4 +106,14 @@ fun rectangleBounds(rectangles: List<Rectangle>): Rectangle {
         maxY = Math.max(maxY, it.y + it.height)
     }
     return Rectangle(Vector2(minX, minY), maxX - minX, maxY - minY)
+}
+
+/** determines of [a] and [b] intersect */
+fun intersects(a: Rectangle, b: Rectangle): Boolean {
+    val above = a.y + a.height < b.height
+    val below = a.y > b.y + b.height
+    val rightOf = a.x > b.x + b.width
+    val leftOf = a.x + a.width < b.x
+
+    return !(above || below || leftOf || rightOf)
 }
