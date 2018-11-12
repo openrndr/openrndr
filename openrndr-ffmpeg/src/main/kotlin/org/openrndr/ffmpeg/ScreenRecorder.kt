be1ec8ff30116dbbbb64d949ee84f353057ab0b4
Edwin Jakobs <edwin@rndr.studio>
2018-11-12 12:56:50 +0100

    ScreenRecorder now sets a custom Program.clock based on frames

--- openrndr-ffmpeg/src/main/kotlin/org/openrndr/ffmpeg/ScreenRecorder.kt
+++ openrndr-ffmpeg/src/main/kotlin/org/openrndr/ffmpeg/ScreenRecorder.kt
@@ -18,8 +18,16 @@ class ScreenRecorder : Extension {
 
     var frameRate = 30
     var profile = MP4Profile()
+    var frameClock = true
 
+    private var frameIndex: Long = 0
     override fun setup(program: Program) {
+
+        if (frameClock) {
+            program.clock = {
+                frameIndex / frameRate.toDouble()
+            }
+        }
 
         fun Int.z(zeroes: Int = 2): String {
             val sv = this.toString()
@@ -35,7 +43,10 @@ class ScreenRecorder : Extension {
             depthBuffer()
         }
         val dt = LocalDateTime.now()
-        videoWriter = VideoWriter().profile(profile).output("${program.javaClass.simpleName}-${dt.year.z(4)}-${dt.month.value.z()}-${dt.dayOfMonth.z()}-${dt.hour.z()}.${dt.minute.z()}.${dt.second.z()}.mp4").size(program.width, program.height).frameRate(frameRate).start()
+
+        val basename = program.javaClass.simpleName.ifBlank { program.window.title }
+        val filename = "$basename-${dt.year.z(4)}-${dt.month.value.z()}-${dt.dayOfMonth.z()}-${dt.hour.z()}.${dt.minute.z()}.${dt.second.z()}.mp4"
+        videoWriter = VideoWriter().profile(profile).output(filename).size(program.width, program.height).frameRate(frameRate).start()
     }
 
     override fun beforeDraw(drawer: Drawer, program: Program) {
@@ -55,5 +66,6 @@ class ScreenRecorder : Extension {
             drawer.view = Matrix44.IDENTITY
             drawer.image(frame.colorBuffer(0))
         }
+        frameIndex++
     }
 }
