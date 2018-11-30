8d508d8e8418ec745c303ccd1b30ad4910cd2497
Edwin Jakobs <edwin@rndr.studio>
2018-11-30 19:06:45 +0100

    Add vector4 minus, times, div, toDoubleArray()

--- openrndr-math/src/main/kotlin/org/openrndr/math/Vector4.kt
+++ openrndr-math/src/main/kotlin/org/openrndr/math/Vector4.kt
@@ -3,6 +3,14 @@ package org.openrndr.math
 import java.io.Serializable
 
 data class Vector4(val x: Double, val y: Double, val z: Double, val w: Double) : Serializable {
+
+    val xy: Vector2 get() = Vector2(x, y)
+    val yx: Vector2 get() = Vector2(y, x)
+    val xz: Vector2 get() = Vector2(x, z)
+    val yz: Vector2 get() = Vector2(y, z)
+    val zx: Vector2 get() = Vector2(z, x)
+    val zy: Vector2 get() = Vector2(z, y)
+
     val xyz: Vector3 get() = Vector3(x, y, z)
     val div: Vector3 get() = Vector3(x / w, y / w, z / w)
 
@@ -24,7 +32,26 @@ data class Vector4(val x: Double, val y: Double, val z: Double, val w: Double) :
             }
 
     operator fun unaryMinus() = Vector4(-x, -y, -z, -w)
+
+    operator fun plus(v: Vector4) = Vector4(x + v.x, y + v.y, z + v.z, w + v.w)
+    operator fun minus(v: Vector4) = Vector4(x - v.x, y - v.y, z - v.z, w - v.w)
+    operator fun times(v: Vector4) = Vector4(x * v.x, y * v.y, z * v.z, w * v.w)
     operator fun times(s: Double) = Vector4(x * s, y * s, z * s, w * s)
+    operator fun div(v: Vector4) = Vector4(x / v.x, y / v.y, z / v.z, w / v.w)
+    operator fun div(s: Double) = Vector4(x / s, y / s, z / s, w / s)
+
+    private operator fun get(i: Int): Double {
+        return when (i) {
+            0 -> x
+            1 -> y
+            2 -> z
+            3 -> w
+            else -> throw IllegalArgumentException("unsupported index")
+        }
+    }
+
+    fun toDoubleArray() = doubleArrayOf(x, y, z, w)
+
 }
 
 operator fun Double.times(v: Vector4) = v * this
