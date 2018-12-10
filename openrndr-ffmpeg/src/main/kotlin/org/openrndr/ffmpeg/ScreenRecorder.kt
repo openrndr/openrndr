e352fc757da73b82e9bc74f3f50925f449e96bbe
Edwin Jakobs <edwin@rndr.studio>
2018-12-10 14:24:16 +0100

    Fixed inversion of ifExist

--- openrndr-ffmpeg/src/main/kotlin/org/openrndr/ffmpeg/ScreenRecorder.kt
+++ openrndr-ffmpeg/src/main/kotlin/org/openrndr/ffmpeg/ScreenRecorder.kt
@@ -72,7 +72,7 @@ class ScreenRecorder : Extension {
         val filename = outputFile?: "video/$basename-${dt.year.z(4)}-${dt.month.value.z()}-${dt.dayOfMonth.z()}-${dt.hour.z()}.${dt.minute.z()}.${dt.second.z()}.mp4"
 
         File(filename).parentFile.let {
-            if (it.exists()) {
+            if (!it.exists()) {
                 it.mkdirs()
             }
         }
