68b0d3fe96efe0343fbe1e277fe0edc7591ad01b
Edwin Jakobs <edwin@rndr.studio>
2019-11-19 23:19:54 +0100

    Add VertexBuffer.createFromFloats

--- openrndr-core/src/main/kotlin/org/openrndr/draw/VertexBuffer.kt
+++ openrndr-core/src/main/kotlin/org/openrndr/draw/VertexBuffer.kt
@@ -6,17 +6,35 @@ import java.nio.ByteBuffer
 interface VertexBuffer {
     companion object {
         fun createDynamic(format: VertexFormat, vertexCount: Int): VertexBuffer = Driver.instance.createDynamicVertexBuffer(format, vertexCount)
-        //fun createStatic(format: VertexFormat, buffer:Buffer):VertexBuffer
+
+        fun createFromFloats(format: VertexFormat, data: FloatArray): VertexBuffer {
+            require((data.size * 4) % format.size == 0) {
+                "supplied data size doesn't match format size"
+            }
+            val vertexBuffer = createDynamic(format, (data.size * 4) / format.size)
+
+            vertexBuffer.put {
+                write(data)
+            }
+            Session.active.track(vertexBuffer)
+            return vertexBuffer
+        }
     }
 
     val vertexFormat: VertexFormat
     val vertexCount: Int
 
+    /**
+     * Gives a read/write shadow for the vertex buffer
+     */
     val shadow: VertexBufferShadow
 
     fun write(data: ByteBuffer, offset: Int = 0)
     fun read(data: ByteBuffer, offset: Int = 0)
 
+    /**
+     * Destroy the vertex buffer
+     */
     fun destroy()
 
     fun put(putter: BufferWriter.() -> Unit): Int {
@@ -33,6 +51,11 @@ interface VertexBuffer {
     }
 }
 
+/**
+ * VertexBuffer builder function.
+ * @param vertexFormat a VertexFormat object that describes the vertex layout
+ * @param vertexCount the number of vertices the vertex buffer should hold
+ */
 fun vertexBuffer(vertexFormat: VertexFormat, vertexCount: Int): VertexBuffer {
     val vertexBuffer = VertexBuffer.createDynamic(vertexFormat, vertexCount)
     Session.active.track(vertexBuffer)
