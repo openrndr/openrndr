0e2d5eea2998c3079bc53c11cc62ec3218771071
Edwin Jakobs <edwin@rndr.studio>
2019-11-30 00:07:45 +0100

    Decommission FFMpegVideoPlayer Closes #44

--- openrndr-ffmpeg/src/main/java/org/openrndr/ffmpeg/adopted/FrameConverter.java
+++
@@ -1,8 +0,0 @@
-package org.openrndr.ffmpeg.adopted;
-
-public abstract class FrameConverter<F> {
-    protected Frame frame;
-
-	public abstract Frame convert(F f);
-	public abstract F convert(Frame frame);
-}
