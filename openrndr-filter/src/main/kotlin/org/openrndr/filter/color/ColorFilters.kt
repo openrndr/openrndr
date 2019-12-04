d711c4108cb5eee7707b5dc31de170b8e49fb1d4
Edwin Jakobs <edwin@rndr.studio>
2019-12-04 11:32:53 +0100

    Add documentation strings for color filters

--- openrndr-filter/src/main/kotlin/org/openrndr/filter/color/ColorFilters.kt
+++ openrndr-filter/src/main/kotlin/org/openrndr/filter/color/ColorFilters.kt
@@ -3,8 +3,19 @@ package org.openrndr.filter.color
 import org.openrndr.draw.*
 import org.openrndr.filter.filterFragmentCode
 
+/**
+ * Converts from sRGB to linear RGB
+ */
 class Linearize : Filter(Shader.createFromCode(Filter.filterVertexCode, filterFragmentCode("color/linearize.frag")))
+
+/**
+ * Converts from linear RGB to sRGB
+ */
 class Delinearize : Filter(Shader.createFromCode(Filter.filterVertexCode, filterFragmentCode("color/delinearize.frag")))
+
+/**
+ * Converts from linear RGB to HybridLogGamma
+ */
 class HybridLogGamma : Filter(Shader.createFromCode(Filter.filterVertexCode, filterFragmentCode("color/hybrid-log-gamma.frag")))
 
 val hybridLogGamma by lazy { HybridLogGamma().apply { untrack() } }
