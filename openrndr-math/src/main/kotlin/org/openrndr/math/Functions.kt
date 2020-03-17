4fb5c4d94111a832041a593c84d58adcffcf2c41
Edwin Jakobs <edwin@rndr.studio>
2020-03-17 16:12:21 +0100

    Add math utility functions

--- openrndr-math/src/main/kotlin/org/openrndr/math/Functions.kt
+++ openrndr-math/src/main/kotlin/org/openrndr/math/Functions.kt
@@ -10,4 +10,83 @@ fun mod(a: Int, b: Int) = ((a % b) + b) % b
 fun mod(a: Float, b: Float) = ((a % b) + b) % b
 fun mod(a: Long, b: Long) = ((a % b) + b) % b
 
+@JvmName("modDouble")
+fun Double.mod_(b: Double) = mod(this, b)
+
+@JvmName("modInt")
+fun Int.mod_(b: Int) = mod(this, b)
+
+@JvmName("modFloat")
+fun Float.mod_(b: Float) = mod(this, b)
+
+@JvmName("modLong")
+fun Long.mod_(b: Long) = mod(this, b)
+
+fun Vector2.mod(b: Vector2) =
+        Vector2(x.mod_(b.x),
+                y.mod_(b.y))
+
+fun Vector3.mod(b: Vector3) =
+        Vector3(x.mod_(b.x),
+                y.mod_(b.y),
+                z.mod_(b.z))
+
+fun Vector4.mod(b: Vector4) =
+        Vector4(x.mod_(b.x),
+                y.mod_(b.y),
+                z.mod_(b.z),
+                w.mod_(b.w))
+
+fun IntVector2.mod(b: IntVector2) =
+        IntVector2(x.mod_(b.x),
+                y.mod_(b.y))
+
+fun IntVector3.mod(b: IntVector3) =
+        IntVector3(x.mod_(b.x),
+                y.mod_(b.y),
+                z.mod_(b.z))
+
+fun IntVector4.mod(b: IntVector4) =
+        IntVector4(x.mod_(b.x),
+                y.mod_(b.y),
+                z.mod_(b.z),
+                w.mod_(b.w))
+
 fun clamp(value: Double, min: Double, max: Double) = max(min, min(max, value))
+fun clamp(value: Int, min: Int, max: Int) = max(min, min(max, value))
+
+@JvmName("doubleClamp")
+fun Double.clamp(min: Double, max: Double) = clamp(this, min, max)
+
+@JvmName("intClamp")
+fun Int.clamp(min: Int, max: Int) = clamp(this, min, max)
+
+fun Vector2.clamp(min : Vector2, max : Vector2) =
+        Vector2(x.clamp(min.x, max.x),
+                y.clamp(min.y, max.y))
+
+fun Vector3.clamp(min : Vector3, max : Vector3) =
+        Vector3(x.clamp(min.x, max.x),
+                y.clamp(min.y, max.y),
+                z.clamp(min.y, max.z))
+
+fun Vector4.clamp(min : Vector4, max : Vector4) =
+        Vector4(x.clamp(min.x, max.x),
+                y.clamp(min.y, max.y),
+                z.clamp(min.z, max.z),
+                w.clamp(min.w, max.w))
+
+fun IntVector2.clamp(min : IntVector2, max : IntVector2) =
+        IntVector2(x.clamp(min.x, max.x),
+                y.clamp(min.y, max.y))
+
+fun IntVector3.clamp(min : IntVector3, max : IntVector3) =
+        IntVector3(x.clamp(min.x, max.x),
+                y.clamp(min.y, max.y),
+                z.clamp(min.y, max.z))
+
+fun IntVector4.clamp(min : IntVector4, max : IntVector4) =
+        IntVector4(x.clamp(min.x, max.x),
+                y.clamp(min.y, max.y),
+                z.clamp(min.z, max.z),
+                w.clamp(min.w, max.w))
--- openrndr-math/src/main/kotlin/org/openrndr/math/Mapping.kt
+++ openrndr-math/src/main/kotlin/org/openrndr/math/Mapping.kt
@@ -19,6 +19,42 @@ fun map(beforeLeft: Double, beforeRight: Double, afterLeft: Double, afterRight: 
     return afterLeft + n * (afterRight - afterLeft)
 }
 
+/**
+ * Linearly maps a value, which is given in the before domain to a value in the after domain
+ * @param beforeLeft the left value of the before domain
+ * @param beforeRight the right value of the before domain
+ * @param afterLeft the left value of the after domain
+ * @param afterRight the right value of the after domain
+ * @return a value in the after domain
+ */
+@JvmName("doubleMap")
+fun Double.map(beforeLeft: Double, beforeRight: Double, afterLeft: Double, afterRight: Double): Double {
+    return map(beforeLeft, beforeRight, afterLeft, afterRight, this)
+}
+
+fun Vector2.map(beforeLeft: Vector2,
+                beforeRight: Vector2,
+                afterLeft: Vector2,
+                afterRight: Vector2) =
+        Vector2(x.map(beforeLeft.x, beforeRight.x, afterLeft.x, afterRight.x),
+                y.map(beforeLeft.y, beforeRight.y, afterLeft.y, afterRight.y))
+
+fun Vector3.map(beforeLeft: Vector3,
+                beforeRight: Vector3,
+                afterLeft: Vector3,
+                afterRight: Vector3) =
+        Vector3(x.map(beforeLeft.x, beforeRight.x, afterLeft.x, afterRight.x),
+                y.map(beforeLeft.y, beforeRight.y, afterLeft.y, afterRight.y),
+                z.map(beforeLeft.z, beforeRight.z, afterLeft.z, afterRight.z))
+
+fun Vector4.map(beforeLeft: Vector4,
+                beforeRight: Vector4,
+                afterLeft: Vector4,
+                afterRight: Vector4) =
+        Vector4(x.map(beforeLeft.x, beforeRight.x, afterLeft.x, afterRight.x),
+                y.map(beforeLeft.y, beforeRight.y, afterLeft.y, afterRight.y),
+                z.map(beforeLeft.z, beforeRight.z, afterLeft.z, afterRight.z),
+                w.map(beforeLeft.w, beforeRight.w, afterLeft.w, afterRight.w))
 
 fun linearstep(edge0: Double, edge1: Double, x: Double): Double = saturate((x - edge0) / (edge1 - edge0))
 
@@ -35,6 +71,24 @@ fun smoothstep(edge0: Double, edge1: Double, x: Double): Double {
     // Evaluate polynomial
     return u * u * (3 - 2 * u)
 }
+
+@JvmName("doubleSmoothstep")
+fun Double.smoothstep(edge0: Double, edge1: Double) = smoothstep(edge0, edge1, this)
+
+fun Vector2.smoothstep(edge0: Vector2, edge1: Vector2): Vector2 =
+        Vector2(this.x.smoothstep(edge0.x, edge1.x),
+                this.x.smoothstep(edge0.y, edge1.y))
+
+fun Vector3.smoothstep(edge0: Vector3, edge1: Vector3): Vector3 =
+        Vector3(this.x.smoothstep(edge0.x, edge1.x),
+                this.x.smoothstep(edge0.y, edge1.y),
+                this.x.smoothstep(edge0.z, edge1.z))
+
+fun Vector4.smoothstep(edge0: Vector4, edge1: Vector4): Vector4 =
+        Vector4(this.x.smoothstep(edge0.x, edge1.x),
+                this.x.smoothstep(edge0.y, edge1.y),
+                this.x.smoothstep(edge0.z, edge1.z),
+                this.x.smoothstep(edge0.w, edge1.w))
 
 /**
  * Smoothstep
@@ -69,5 +123,14 @@ fun smootherstep(edge0: Double, edge1: Double, x: Double): Double {
     return u * u * u * (u * (u * 6 - 15) + 10)
 }
 
+
 fun saturate(x: Double) = max(0.0, min(1.0, x))
+
+@JvmName("doubleSaturate")
+fun Double.saturate() = saturate(this)
+
+fun Vector2.saturate() = Vector2(x.saturate(), y.saturate())
+fun Vector3.saturate() = Vector3(x.saturate(), y.saturate(), z.saturate())
+fun Vector4.saturate() = Vector4(x.saturate(), y.saturate(), z.saturate(), w.saturate())
+
 fun mix(left: Double, right: Double, x: Double) = left * (1.0 - x) + right * x
