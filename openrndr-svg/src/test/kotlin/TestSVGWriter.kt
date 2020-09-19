194faf4741cc1ed068956b844fdf29a7d964efe0
Edwin Jakobs <edwin@rndr.studio>
2020-09-19 08:44:37 +0200

    Add SVGWriter tests

--- openrndr-svg/src/test/kotlin/TestSVGWriter.kt
+++ openrndr-svg/src/test/kotlin/TestSVGWriter.kt
@@ -1,7 +1,9 @@
 package org.openrndr.math
 
 import org.openrndr.shape.CompositionDrawer
-import org.openrndr.shape.compound
+import org.openrndr.shape.drawComposition
+import org.openrndr.svg.loadSVG
+import org.openrndr.svg.toSVG
 import org.openrndr.svg.writeSVG
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
@@ -12,7 +14,7 @@ object TestSVGWriter : Spek({
         val cd = CompositionDrawer()
         cd.group("Layer_2") {
             val circle = cd.circle(Vector2(100.0, 100.0), 40.0)
-            circle.attributes["openrndr:test"] = "5"
+            circle?.attributes?.set("openrndr:test", "5")
         }
         val comp = cd.composition
         it("can be written to SVG string") {
@@ -20,4 +22,18 @@ object TestSVGWriter : Spek({
             println(t)
         }
     }
+    describe("a composition with transforms") {
+        val comp = drawComposition {
+            group {
+                translate(Vector2(50.0, 50.0))
+                circle(Vector2(100.0, 100.0), 50.0)?.id = "circle"
+            }
+        }
+        it("can be written and loaded") {
+            val svgString = comp.toSVG()
+            val loaded = loadSVG(svgString)
+            loaded
+        }
+    }
+
 })
