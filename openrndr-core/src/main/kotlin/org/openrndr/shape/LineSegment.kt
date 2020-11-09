b470e76b37b2b4b956e75575628ac5823308572f
Edwin Jakobs <edwin@rndr.studio>
2020-11-09 16:36:29 +0100

    Improve documentation of LineSegment, remove unused functions

--- openrndr-core/src/main/kotlin/org/openrndr/shape/LineSegment.kt
+++ openrndr-core/src/main/kotlin/org/openrndr/shape/LineSegment.kt
@@ -2,6 +2,7 @@
 
 package org.openrndr.shape
 
+import org.openrndr.math.LinearType
 import org.openrndr.math.Vector2
 import org.openrndr.math.YPolarity
 import org.openrndr.math.map
@@ -9,14 +10,28 @@ import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
 
+/**
+ * a 2D line segment
+ * @param start start of the line segment
+ * @param end end of the line segment
+ */
 @Suppress("MemberVisibilityCanBePrivate", "unused")
-data class LineSegment(val start: Vector2, val end: Vector2) {
-
+data class LineSegment(val start: Vector2, val end: Vector2) : LinearType<LineSegment> {
     constructor(x0: Double, y0: Double, x1: Double, y1: Double) : this(Vector2(x0, y0), Vector2(x1, y1))
-
+    /**
+     * direction of the line segment
+     */
     val direction get() = (end - start)
+
+    /**
+     * normal of the line segment, a unit length vector
+     */
     val normal get() = (end - start).normalized.perpendicular(YPolarity.CW_NEGATIVE_Y)
 
+    /**
+     * find point on the line segment that is nearest to the query point
+     * @param query the query point
+     */
     fun nearest(query: Vector2): Vector2 {
         val l2 = end.minus(start).squaredLength
         if (l2 == 0.0) return start
@@ -27,60 +42,17 @@ data class LineSegment(val start: Vector2, val end: Vector2) {
                 start.y + t * (end.y - start.y))
     }
 
-    fun distance(query: Vector2): Double {
-        val l2 = end.minus(start).squaredLength
-        if (l2 == 0.0) return query.minus(start).length
+    /**
+     * find distance between the query point and the point on the line segment that is nearest to the query point
+     * @param query the query point
+     */
+    fun distance(query: Vector2): Double = nearest(query).distanceTo(query)
 
-        var t = ((query.x - start.x) * (end.x - start.x) + (query.y - start.y) * (end.y - start.y)) / l2
-        t = max(0.0, min(1.0, t))
-        return (query - Vector2(start.x + t * (end.x - start.x),
-                start.y + t * (end.y - start.y))).length
-    }
-
-    fun endingAtY(y: Double): LineSegment {
-        val dy = end.y - start.y
-        val y0 = y - start.y
-        val t = y0 / dy
-        return LineSegment(start, start + (end - start) * t)
-    }
-
-    fun startingAtY(y: Double): LineSegment {
-        val dy = end.y - start.y
-        val y0 = y - start.y
-        val t = y0 / dy
-        return LineSegment(start + (end - start) * t, end)
-    }
-
-    fun startingAtX(x: Double): LineSegment {
-        val dx = end.x - start.x
-        val x0 = x - start.x
-        val t = x0 / dx
-        return LineSegment(start + (end - start) * t, end)
-    }
-
-    fun endingAtX(x: Double): LineSegment {
-        val dx = end.x - start.x
-        val x0 = x - start.x
-        val t = x0 / dx
-        return LineSegment(start, start + (end - start) * t)
-    }
-
-    fun endingAtNearest(x: Double, y: Double): LineSegment {
-        val dx = end.x - start.x
-        val x0 = x - start.x
-        val tx = x0 / dx
-
-        val dy = end.y - start.y
-        val y0 = y - start.y
-        val ty = y0 / dy
-
-        return if (tx == tx && tx < ty) {
-            LineSegment(start, start + (end - start) * tx)
-        } else {
-            LineSegment(start, start + (end - start) * ty)
-        }
-    }
-
+    /**
+     * calculate a sub line segment from t0 to t1
+     * @param t0 t-parameter value at start of line segment
+     * @param t1 t-parameter value at end of line segment
+     */
     fun sub(t0: Double, t1: Double): LineSegment {
         var z0 = t0
         var z1 = t1
@@ -96,20 +68,33 @@ data class LineSegment(val start: Vector2, val end: Vector2) {
         }
     }
 
+    /**
+     * split line segment at t-parameter value
+     * @param t the t-parameter value between 0 and 1
+     */
     fun split(t: Double): Array<LineSegment> {
         val u = t.coerceIn(0.0, 1.0)
         val cut = start + (end.minus(start) * u)
         return arrayOf(LineSegment(start, cut), LineSegment(cut, end))
     }
 
+    /**
+     * calculate point on the line segment
+     * @param t t-parameter value between 0 and 1
+     */
     fun position(t: Double) = start + (end.minus(start) * t)
 
-    fun rotate(theta: Double, t: Double = 0.5): LineSegment {
+    /**
+     * rotate line segment around a point on the line segment
+     * @param degrees rotation in degrees
+     * @param t t-parameter value of point on line segment to rotate around, default is 0.5 (mid-point)
+     */
+    fun rotate(degrees: Double, t: Double = 0.5): LineSegment {
         val anchorPoint = end.mix(start, t.coerceIn(0.0, 1.0))
 
         return LineSegment(
-                start.rotate(theta, anchorPoint),
-                end.rotate(theta, anchorPoint)
+                start.rotate(degrees, anchorPoint),
+                end.rotate(degrees, anchorPoint)
         )
     }
 
@@ -117,36 +102,58 @@ data class LineSegment(val start: Vector2, val end: Vector2) {
         return LineSegment(start - direction * times, end + direction * times)
     }
 
+    /**
+     * convert line segment to [Segment]
+     */
     val segment: Segment
         get() = Segment(start, end)
 
+    /**
+     * convert line segment to [ShapeContour]
+     */
     val contour: ShapeContour
         get() = ShapeContour.fromPoints(listOf(start, end), false, YPolarity.CW_NEGATIVE_Y)
 
+    /**
+     * convert line segment to [Shape]
+     */
     val shape: Shape
         get() = Shape(listOf(contour))
 
-    operator fun times(scale: Double) : LineSegment {
+    override operator fun times(scale: Double): LineSegment {
         return LineSegment(start * scale, end * scale)
     }
 
-    operator fun div(scale: Double) : LineSegment {
+    override operator fun div(scale: Double): LineSegment {
         return LineSegment(start / scale, end / scale)
     }
 
-    operator fun plus(right: LineSegment) : LineSegment {
+    override operator fun plus(right: LineSegment): LineSegment {
         return LineSegment(start + right.start, end + right.end)
     }
 
-    operator fun minus(right: LineSegment) : LineSegment {
+    override operator fun minus(right: LineSegment): LineSegment {
         return LineSegment(start - right.start, end - right.end)
     }
-
 }
 
+/**
+ * find intersection between two line segments
+ * @param a the first line segment
+ * @param b the second line segment
+ * @param eps how far outside the t-parameter range of 0 .. 1 are intersections considered
+ */
 fun intersection(a: LineSegment, b: LineSegment, eps: Double = 0.0): Vector2 =
         intersection(a.start, a.end, b.start, b.end, eps)
 
+/**
+ * find intersection between two line segments
+ * @param a0 the start of the first line segment
+ * @param a1 the end of the first line segment
+ * @param b0 the start of the second line segment
+ * @param b1 the end of the second line segment
+ * @param eps how far outside the t-parameter range of 0 .. 1 are intersections considered, default is 0.0
+ */
 fun intersection(a0: Vector2, a1: Vector2, b0: Vector2, b1: Vector2, eps: Double = 0.0): Vector2 {
     val x0 = a0.x
     val x1 = a1.x
