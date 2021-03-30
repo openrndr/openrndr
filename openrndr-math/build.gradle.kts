8b2c27ba412f17b6664b9060ee380c5dd6fae004
Kazik Pogoda <morisil@xemantic.com>
2021-03-30 11:01:42 +0200

    kotlin bumped to 1.4.32, kotlinx-serialization and coroutines in sync. Kotlin loggings is already mpp and does not need -jvm suffix. The same for stdlib. native build switched off. Kotlin JS libs build for both - IR and LEGACY, in hope that it will fix the metadata problem + ColorRGBaTest.kt

--- openrndr-math/build.gradle.kts
+++ openrndr-math/build.gradle.kts
@@ -24,22 +24,28 @@ kotlin {
         nodejs()
     }
 
+    // native part switched off for now as it's quite unstable at the beginning on 2021
+    /*
     val hostOs = System.getProperty("os.name")
     val isMingwX64 = hostOs.startsWith("Windows")
+    @Suppress("UNUSED_VARIABLE")
     val nativeTarget = when {
         hostOs == "Mac OS X" -> macosX64("native")
         hostOs == "Linux" -> linuxX64("native")
         isMingwX64 -> mingwX64("native")
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
     }
+     */
 
     sourceSets {
 
+        @Suppress("UNUSED_VARIABLE")
         val commonMain by getting {
             dependencies {
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:$kotlinxSerializationVersion")
             }
         }
+        @Suppress("UNUSED_VARIABLE")
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test-common"))
@@ -49,7 +55,9 @@ kotlin {
             }
         }
 
+        @Suppress("UNUSED_VARIABLE")
         val jvmMain by getting
+        @Suppress("UNUSED_VARIABLE")
         val jvmTest by getting {
             dependencies {
                 implementation(kotlin("test-common"))
@@ -61,16 +69,22 @@ kotlin {
             }
         }
 
+        @Suppress("UNUSED_VARIABLE")
         val jsMain by getting
+        @Suppress("UNUSED_VARIABLE")
         val jsTest by getting {
             dependencies {
                 implementation(kotlin("test-js"))
             }
         }
 
+    // native part switched off for now as it's quite unstable at the beginning on 2021
+    /*
+        @Suppress("UNUSED_VARIABLE")
         val nativeMain by getting
+        @Suppress("UNUSED_VARIABLE")
         val nativeTest by getting
-
+     */
     }
 
 }
