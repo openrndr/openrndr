3e6557480d24a57cabcee930867de8d7103340cb
Edwin Jakobs <edwin@rndr.studio>
2021-04-02 07:14:15 +0200

    Fix jsMain Clock

--- openrndr-animatable/src/jsMain/kotlin/org/openrndr/animatable/Clock.kt
+++ openrndr-animatable/src/jsMain/kotlin/org/openrndr/animatable/Clock.kt
@@ -1,11 +1,8 @@
 package org.openrndr.animatable
 
-import kotlin.js.Date
+import kotlinx.browser.window
 
 actual class DefaultClock : Clock {
-    override val timeNanos: Long get() = time * 1000
-
-    override val time: Long get() {
-        return Date().getMilliseconds().toLong()
-    }
+    override val timeNanos: Long get() = (window.performance.now() * 1000).toLong()
+    override val time: Long get() = timeNanos / 1000
 }
