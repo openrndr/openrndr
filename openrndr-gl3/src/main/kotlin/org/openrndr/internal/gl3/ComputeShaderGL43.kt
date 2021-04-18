71e9bd018d671c7a7138d739854f1cac8f197a7f
Weili Shi <me@shi-weili.com>
2021-04-18 10:03:21 -0400

    Add support to other ImageBinding types to ComputeShader

--- openrndr-gl3/src/main/kotlin/org/openrndr/internal/gl3/ComputeShaderGL43.kt
+++ openrndr-gl3/src/main/kotlin/org/openrndr/internal/gl3/ComputeShaderGL43.kt
@@ -59,14 +59,20 @@ class ComputeShaderGL43(val programObject: Int, val name: String = "compute_shad
         require((Driver.instance as DriverGL3).version >= DriverVersionGL.VERSION_4_3)
 
         when (imageBinding) {
+            is BufferTextureImageBinding -> {
+                val bufferTexture = imageBinding.bufferTexture as BufferTextureGL3
+                require(bufferTexture.format.componentCount != 3) {
+                    "color buffer has unsupported format (${imageBinding.bufferTexture.format}), only formats with 1, 2 or 4 components are supported"
+                }
+                GL43C.glBindImageTexture(image, bufferTexture.texture, imageBinding.level, false, 0, imageBinding.access.gl(), bufferTexture.glFormat())
+            }
             is ColorBufferImageBinding -> {
                 val colorBuffer = imageBinding.colorBuffer as ColorBufferGL3
                 require(colorBuffer.format.componentCount != 3) {
                     "color buffer has unsupported format (${imageBinding.colorBuffer.format}), only formats with 1, 2 or 4 components are supported"
                 }
-                GL43C.glBindImageTexture(image, colorBuffer.texture, 0, false, 0, imageBinding.access.gl(), colorBuffer.glFormat())
+                GL43C.glBindImageTexture(image, colorBuffer.texture, imageBinding.level, false, 0, imageBinding.access.gl(), colorBuffer.glFormat())
             }
-
             is ArrayTextureImageBinding -> {
                 val arrayTexture = imageBinding.arrayTexture as ArrayTextureGL3
                 require(arrayTexture.format.componentCount != 3) {
@@ -74,9 +80,31 @@ class ComputeShaderGL43(val programObject: Int, val name: String = "compute_shad
                 }
                 GL43C.glBindImageTexture(image, arrayTexture.texture, imageBinding.level, false, 0, imageBinding.access.gl(), arrayTexture.glFormat())
             }
+            is CubemapImageBinding -> {
+                val cubemap = imageBinding.cubemap as CubemapGL3
+                require(cubemap.format.componentCount != 3) {
+                    "color buffer has unsupported format (${imageBinding.cubemap.format}), only formats with 1, 2 or 4 components are supported"
+                }
+                GL43C.glBindImageTexture(image, cubemap.texture, imageBinding.level, false, 0, imageBinding.access.gl(), cubemap.glFormat())
+            }
+            is ArrayCubemapImageBinding -> {
+                val arrayCubemap = imageBinding.arrayCubemap as ArrayCubemapGL4
+                require(arrayCubemap.format.componentCount != 3) {
+                    "color buffer has unsupported format (${imageBinding.arrayCubemap.format}), only formats with 1, 2 or 4 components are supported"
+                }
+                GL43C.glBindImageTexture(image, arrayCubemap.texture, imageBinding.level, false, 0, imageBinding.access.gl(), arrayCubemap.glFormat())
+            }
+            is VolumeTextureImageBinding -> {
+                val volumeTexture = imageBinding.volumeTexture as VolumeTextureGL3
+                require(volumeTexture.format.componentCount != 3) {
+                    "color buffer has unsupported format (${imageBinding.volumeTexture.format}), only formats with 1, 2 or 4 components are supported"
+                }
+                GL43C.glBindImageTexture(image, volumeTexture.texture, imageBinding.level, false, 0, imageBinding.access.gl(), volumeTexture.glFormat())
+            }
 
             else -> error("unsupported binding")
         }
+        checkGLErrors()
         val index = uniformIndex(name)
         GL43C.glUniform1i(index, image)
         checkGLErrors()
