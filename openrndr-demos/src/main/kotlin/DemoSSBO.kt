8576ffffcee26cc839e573567b3595f26bdf2627
Abe Pazos <abe@hamoid.com>
2021-04-22 19:41:58 +0300

    Add comments to DemoSSBO, show multiple results (#228)

--- openrndr-demos/src/main/kotlin/DemoSSBO.kt
+++ openrndr-demos/src/main/kotlin/DemoSSBO.kt
@@ -1,6 +1,5 @@
 import org.intellij.lang.annotations.Language
 import org.openrndr.application
-import org.openrndr.collections.pmap
 import org.openrndr.draw.*
 import org.openrndr.math.IntVector3
 import org.openrndr.math.Vector2
@@ -9,7 +8,20 @@ import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import kotlin.random.Random
 
-// TODO: Find a fix for reading back struct
+// A Shader Storage Buffer Object is a Buffer Object that is used to
+// store and retrieve data from within the OpenGL Shading Language.
+
+// https://www.khronos.org/opengl/wiki/Shader_Storage_Buffer_Object
+
+// This demo uses two SSBOs: the first one is used as data for calculations,
+// the second one is the result which is read back from GPU to CPU.
+
+// The compute shader is executed `csWidth` times in parallel.
+// Each instance uses the same input data and outputs different results
+// by dividing the data by the shader execution instance ID.
+
+// Note that reading data back to the CPU is optional.
+
 fun main() = application {
     configure {
         width = 720
@@ -34,6 +46,7 @@ fun main() = application {
                 vec3 position;
             };
             
+            // SSBOs are declared as using the `buffer` keyword
             layout (std430, binding = 0) buffer Block1 {
                 vec2 trap[8];
                 Particle particles[4];
@@ -46,21 +59,25 @@ fun main() = application {
             
             void main(void) {
                 ivec2 id = ivec2(gl_GlobalInvocationID.xy);
-                int idx = 0;
+                int idx = id.x * 6;
+                float k =  1.0 + id.x;
+                          
+                results[idx + 0] = particles[0].position.x / k;
+                results[idx + 1] = particles[0].position.y / k;
+                results[idx + 2] = particles[0].position.z / k;
                 
-                results[idx++] = particles[0].position.x;
-                results[idx++] = particles[0].position.y;
-                results[idx++] = particles[0].position.z;
-                
-                results[idx++] = agents[0].position.x;
-                results[idx++] = agents[0].position.y;
-                results[idx++] = agents[0].position.z;
+                results[idx + 3] = agents[0].position.x / k;
+                results[idx + 4] = agents[0].position.y / k;
+                results[idx + 5] = agents[0].position.z / k;
             }
         """.trimIndent()
 
-        val count = 10
         val cs = ComputeShader.fromCode(shader, "Compute")
+        // number of parallel calculations in the GPU
+        val csWidth = 10
+        val resultLength = 6
 
+        // define structures matching the two `buffer` entries from GLSL
         val block1 = shaderStorageBuffer(shaderStorageFormat {
             member("trap", BufferMemberType.VECTOR2_FLOAT, 8)
             struct("Particle", "particles", 4) {
@@ -75,40 +92,55 @@ fun main() = application {
         })
 
         val block2 = shaderStorageBuffer(shaderStorageFormat {
-            member("results", BufferMemberType.FLOAT, 6)
+            member("results", BufferMemberType.FLOAT, csWidth * resultLength)
         })
 
+        // upload data to the compute shader
         block1.put {
-            for (i in 0 until 8) {
+            // trap
+            for (i in 1..8) {
                 write(Vector2(500.0, 500.0))
             }
 
+            // particles
             for (i in 1..4) {
+                // position
                 write(IntVector3(201 * i, 202 * i, 203 * i))
+                // velocity
                 write(Vector3(420.0 * i))
+                // age
                 write(20f * i)
+                // isActive
                 write(Random.nextBoolean())
             }
 
+            // agents
             for (i in 1..2) {
+                // position
                 write(Vector3(420.0 * i))
             }
         }
 
-        val bb2 = ByteBuffer.allocateDirect(block2.format.size).order(ByteOrder.nativeOrder())
-
+        // bind SSBOs
         cs.buffer("Block1", block1)
         cs.buffer("Block2", block2)
 
-        cs.execute(count, 1, 1)
+        // do calculation
+        cs.execute(csWidth, 1, 1)
 
-        block2.read(bb2)
-        bb2.rewind()
+        // read back results
+        val byteBuffer = ByteBuffer.allocateDirect(block2.format.size)
+            .order(ByteOrder.nativeOrder())
+        block2.read(byteBuffer)
 
+        // show results
         println("Results:")
-
-        for (i in 0 until 6) {
-            print("${bb2.float}, ")
+        byteBuffer.rewind()
+        repeat(csWidth) {
+            repeat(resultLength) {
+                print("${byteBuffer.float}, ")
+            }
+            println()
         }
     }
 }
