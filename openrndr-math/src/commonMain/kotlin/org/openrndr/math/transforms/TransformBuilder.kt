3d42787c3ff36135f0d8a7995dbc9bec2d65413f
vechro <stenmr@gmail.com>
2021-07-22 08:37:51 +0300

    Apply code formatting

--- openrndr-math/src/commonMain/kotlin/org/openrndr/math/transforms/TransformBuilder.kt
+++ openrndr-math/src/commonMain/kotlin/org/openrndr/math/transforms/TransformBuilder.kt
@@ -14,11 +14,11 @@ class TransformBuilder(baseTransform: Matrix44 = Matrix44.IDENTITY) {
         transform *= axis.matrix.matrix44
     }
 
-    fun rotate(axis:Vector3=Vector3.UNIT_Z, degrees:Double) {
+    fun rotate(axis: Vector3 = Vector3.UNIT_Z, degrees: Double) {
         transform *= Matrix44.rotate(axis, degrees)
     }
 
-    fun translate(offset:Vector3) {
+    fun translate(offset: Vector3) {
         transform *= Matrix44.translate(offset)
     }
 
@@ -26,23 +26,23 @@ class TransformBuilder(baseTransform: Matrix44 = Matrix44.IDENTITY) {
         transform *= Matrix44.translate(offset.xy0)
     }
 
-    fun translate(x:Double, y:Double, z:Double = 0.0) {
+    fun translate(x: Double, y: Double, z: Double = 0.0) {
         transform *= Matrix44.translate(Vector3(x, y, z))
     }
 
-    fun scale(scale:Double) {
+    fun scale(scale: Double) {
         transform *= Matrix44.scale(scale, scale, scale)
     }
 
-    fun scale(scaleX:Double, scaleY:Double, scaleZ:Double) {
+    fun scale(scaleX: Double, scaleY: Double, scaleZ: Double) {
         transform *= Matrix44.scale(scaleX, scaleY, scaleZ)
     }
 
-    fun scale(scale:Vector3) {
+    fun scale(scale: Vector3) {
         transform *= Matrix44.scale(scale.x, scale.y, scale.z)
     }
 
-    fun multiply(matrix:Matrix44) {
+    fun multiply(matrix: Matrix44) {
         transform *= matrix
     }
 }
@@ -52,20 +52,21 @@ class TransformBuilder(baseTransform: Matrix44 = Matrix44.IDENTITY) {
  * @param baseTransform the transform to start with, default is an identity matrix
  * @param builder a function that is invoke inside the [TransformBuilder] context
  */
-fun transform(baseTransform: Matrix44 = Matrix44.IDENTITY, builder:TransformBuilder.() -> Unit):Matrix44 {
+fun transform(baseTransform: Matrix44 = Matrix44.IDENTITY, builder: TransformBuilder.() -> Unit): Matrix44 {
     return TransformBuilder(baseTransform).apply { builder() }.transform
 }
 
 /**
  * An alias for transform, useful for those cases in which the word transform is used too often
  */
-fun buildTransform(baseTransform: Matrix44 = Matrix44.IDENTITY, builder: TransformBuilder.() -> Unit) = transform(baseTransform, builder)
+fun buildTransform(baseTransform: Matrix44 = Matrix44.IDENTITY, builder: TransformBuilder.() -> Unit) =
+    transform(baseTransform, builder)
 
 /**
  * Matrix44 transform helper
  */
 @JvmName("matrix44Transform")
-fun Matrix44.transform(builder: TransformBuilder.() -> Unit) : Matrix44 {
+fun Matrix44.transform(builder: TransformBuilder.() -> Unit): Matrix44 {
     return buildTransform(baseTransform = this, builder = builder)
 }
 
