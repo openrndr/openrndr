c54e0fbe1a613dd5a63edd570e770641da1bba5b
Edwin Jakobs <edwin@rndr.studio>
2021-10-21 21:32:38 +0200

    Fix demo

--- openrndr-demos/src/main/kotlin/DemoProgram01.kt
+++ openrndr-demos/src/main/kotlin/DemoProgram01.kt
@@ -3,7 +3,7 @@ package org.openrndr.demos
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
 
-suspend fun main() {
+fun main() {
     application {
         program {
             extend {
