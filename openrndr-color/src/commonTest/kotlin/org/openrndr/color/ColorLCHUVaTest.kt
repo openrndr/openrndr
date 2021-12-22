43e56061ac42955ded7baa62018a6a49bd660b02
Edwin Jakobs <edwin@rndr.studio>
2021-12-22 10:25:21 +0100

    [openrndr-color] Add tests for ColorLCHUVa, ColorLUVa closes #269

--- openrndr-color/src/commonTest/kotlin/org/openrndr/color/ColorLCHUVaTest.kt
+++ openrndr-color/src/commonTest/kotlin/org/openrndr/color/ColorLCHUVaTest.kt
@@ -1,6 +1,7 @@
 package org.openrndr.color
 
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class ColorLCHUVaTest {
@@ -11,5 +12,27 @@ class ColorLCHUVaTest {
         assertTrue(c.l == c.l && c.c == c.c && c.h == c.h)
         val crgb = c.toRGBa()
         assertTrue(crgb.r == crgb.r && crgb.g == crgb.g && crgb.b == crgb.b)
+    }
+
+    @Test
+    fun testIssue269() {
+        // addresses https://github.com/openrndr/openrndr/issues/269
+        val hue = 293.0
+        val lchuv = ColorLCHUVa(4.0, 20.0, hue)
+        val uv = lchuv.toLUVa()
+
+        // here uv is ColorLUVa(l=4.0, u=7.81462256978547, v=-18.41009706904881, alpha=1.0, ref=ColorXYZa(x=0.9505, y=1.0, z=1.089, a=1.0))
+        // http://colormine.org/convert/rgb-to-luv converts this to rgb (62.90, 0.0, 84.32)
+        // which is very close to the srgb values below (due to difference in reference white point)
+
+        val rgb255 = uv.toRGBa().toSRGB() * 255.0
+        assertEquals(rgb255.r, 62.976759000412294, 0.0)
+        assertEquals(rgb255.g, -58.03268354243183, 0.0)
+        assertEquals(rgb255.b, 84.41559049951348, 0.0)
+
+        // OPENRNDR's green value is negative, this is due to the LCHuv/LUV gamut being larger than the sRGB one
+        // to preserve information (and allow to convert back to colorspaces with a supporting gamut) none of the
+        // OPENRNDR conversions clamp
+
     }
 }
--- openrndr-color/src/commonTest/kotlin/org/openrndr/color/ColorLUVaTest.kt
+++ openrndr-color/src/commonTest/kotlin/org/openrndr/color/ColorLUVaTest.kt
@@ -1,6 +1,7 @@
 package org.openrndr.color
 
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class ColorLUVaTest {
@@ -11,5 +12,25 @@ class ColorLUVaTest {
         assertTrue(c.l == c.l && c.u == c.u && c.v == c.v)
         val crgb = c.toRGBa()
         assertTrue(crgb.r == crgb.r && crgb.g == crgb.g && crgb.b == crgb.b)
+    }
+
+    @Test
+    fun testNeutralReferencePoint() {
+        val luvWhite = ColorRGBa.WHITE.toLUVa(ref = ColorXYZa.NEUTRAL)
+        assertEquals(100.0, luvWhite.l, 0.0)
+        assertEquals(0.0, luvWhite.u, 0.0)
+        assertEquals(0.0, luvWhite.v, 0.0)
+
+        val luvBlack = ColorRGBa.BLACK.toLUVa(ref = ColorXYZa.NEUTRAL)
+        assertEquals(0.0, luvBlack.l, 0.0)
+        assertEquals(0.0, luvBlack.u, 0.0)
+        assertEquals(0.0, luvBlack.v, 0.0)
+
+        // these values are very close to the conversions provided by http://colormine.org/convert/rgb-to-luv
+        // differences are due to differing reference white points
+        val luvRed = ColorRGBa.RED.toLUVa(ref = ColorXYZa.NEUTRAL)
+        assertEquals(53.23288178584245, luvRed.l, 0.0)
+        assertEquals(175.05256160740132, luvRed.u, 0.0)
+        assertEquals(37.759612089002886, luvRed.v, 0.0)
     }
 }
