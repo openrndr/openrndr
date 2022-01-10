f94436f7ae6a5b28efa1dcb353cd4c004b622187
Rein van der Woerd <reinvanderwoerd@me.com>
2022-01-10 12:17:49 +0100

    [openrndr-js] Implement missing / additional events (#285)

--- openrndr-js/openrndr-webgl/src/jsMain/kotlin/org/openrndr/webgl/ApplicationWebGL.kt
+++ openrndr-js/openrndr-webgl/src/jsMain/kotlin/org/openrndr/webgl/ApplicationWebGL.kt
@@ -2,15 +2,25 @@ package org.openrndr.webgl
 
 import kotlinx.browser.document
 import kotlinx.browser.window
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.await
 import org.khronos.webgl.WebGLContextAttributes
 import org.khronos.webgl.WebGLRenderingContext
 import org.openrndr.*
 import org.openrndr.draw.Drawer
 import org.openrndr.internal.Driver
 import org.openrndr.math.Vector2
+import org.w3c.dom.HTMLAnchorElement
 import org.w3c.dom.HTMLCanvasElement
+import org.w3c.dom.asList
 import org.w3c.dom.events.WheelEvent
+import org.w3c.files.File
+import org.w3c.files.FileList
+import org.w3c.files.FileReader
+import org.w3c.files.get
+import kotlin.js.Promise
 import org.w3c.dom.events.MouseEvent as HtmlMouseEvent
+import org.w3c.dom.events.KeyboardEvent as HtmlKeyboardEvent
 import kotlin.math.min
 
 val applicationWebGLInitializer = object {
@@ -78,7 +88,34 @@ class ApplicationWebGL(private val program: Program, private val configuration: 
             )
         })
 
+        // Keyboard
+        window.addEventListener("keydown", {
+            it as HtmlKeyboardEvent
+            program.keyboard.pressedKeys.add(it.key)
+            program.keyboard.keyDown.trigger(
+                KeyEvent(
+                    KeyEventType.KEY_DOWN,
+                    it.which,
+                    it.key,
+                    getModifiers(it)
+                )
+            )
+        })
 
+        window.addEventListener("keyup", {
+            it as HtmlKeyboardEvent
+            program.keyboard.pressedKeys.remove(it.key)
+            program.keyboard.keyUp.trigger(
+                KeyEvent(
+                    KeyEventType.KEY_UP,
+                    it.which,
+                    it.key,
+                    getModifiers(it)
+                )
+            )
+        })
+
+        // Mouse
         var lastDragPosition = Vector2.ZERO
         var down = false
         window.addEventListener("mousedown", {
@@ -87,21 +124,56 @@ class ApplicationWebGL(private val program: Program, private val configuration: 
             val x = it.clientX.toDouble()
             val y = it.clientY.toDouble()
             lastDragPosition = Vector2(x, y)
+
+            program.mouse.buttonDown.trigger(
+                MouseEvent(
+                    lastDragPosition,
+                    Vector2.ZERO,
+                    Vector2.ZERO,
+                    MouseEventType.BUTTON_DOWN,
+                    when (it.button as Int) {
+                        0 -> MouseButton.LEFT
+                        1 -> MouseButton.CENTER
+                        2 -> MouseButton.RIGHT
+                        else -> MouseButton.LEFT
+                    },
+                    emptySet()
+                )
+            )
         })
 
         window.addEventListener("mouseup", {
+            it as HtmlMouseEvent
             down = false
+
+            program.mouse.buttonUp.trigger(
+                MouseEvent(
+                    lastDragPosition,
+                    Vector2.ZERO,
+                    Vector2.ZERO,
+                    MouseEventType.BUTTON_UP,
+                    when (it.button as Int) {
+                        0 -> MouseButton.LEFT
+                        1 -> MouseButton.CENTER
+                        2 -> MouseButton.RIGHT
+                        else -> MouseButton.LEFT
+                    },
+                    emptySet()
+                )
+            )
         })
 
         window.addEventListener("wheel", {
             it as WheelEvent
             program.mouse.scrolled.trigger(
-                MouseEvent(cursorPosition,
+                MouseEvent(
+                    cursorPosition,
                     Vector2(it.deltaX, it.deltaY),
                     Vector2.ZERO,
                     MouseEventType.SCROLLED,
                     MouseButton.NONE,
-                    emptySet())
+                    emptySet()
+                )
             )
         })
 
@@ -143,6 +215,29 @@ class ApplicationWebGL(private val program: Program, private val configuration: 
         })
 
 
+        // Drag and drop
+        canvas?.addEventListener("dragover", {
+            it.preventDefault()
+            println("dragover")
+        })
+
+        canvas?.addEventListener("drop", {
+            it.preventDefault()
+            println("drop")
+            val files: FileList = js("it.dataTransfer.files")
+            val promises = files.asList().map { f -> readFileOrBlobAsDataUrl(f)}
+
+            Promise.all(promises.toTypedArray()).then { images ->
+                program.window.drop.trigger(
+                    DropEvent(
+                        this.cursorPosition,
+                        images.toList()
+                    )
+                )
+            }
+        })
+
+
         defaultRenderTarget = ProgramRenderTargetWebGL(context ?: error("no context"), program)
         defaultRenderTarget?.bind()
 
@@ -151,6 +246,25 @@ class ApplicationWebGL(private val program: Program, private val configuration: 
         program.width = dims.x.toInt()
         program.height = dims.y.toInt()
         program.setup()
+    }
+
+    private fun readFileOrBlobAsDataUrl(file: File): Promise<String> {
+        return Promise() { resolve, reject ->
+            val reader = FileReader()
+            reader.readAsDataURL(file);
+            reader.onloadend = {
+                resolve(reader.result)
+            }
+        }
+    }
+
+    private fun getModifiers(e: HtmlKeyboardEvent): Set<KeyModifier> {
+        val set = mutableSetOf<KeyModifier>()
+        if (e.ctrlKey) set.add(KeyModifier.CTRL)
+        if (e.altKey) set.add(KeyModifier.ALT)
+        if (e.metaKey) set.add(KeyModifier.SUPER)
+        if (e.shiftKey) set.add(KeyModifier.SHIFT)
+        return set
     }
 
     override fun loop() {
@@ -172,6 +286,7 @@ class ApplicationWebGL(private val program: Program, private val configuration: 
     override var clipboardContents: String?
         get() = TODO("Not yet implemented")
         set(_) {}
+
     override var windowTitle: String
         get() = window.document.title
         set(text) {
@@ -197,6 +312,7 @@ class ApplicationWebGL(private val program: Program, private val configuration: 
     override var cursorVisible: Boolean
         get() = TODO("Not yet implemented")
         set(_) {}
+
     override var cursorHideMode: MouseCursorHideMode
         get() = TODO("Not yet implemented")
         set(_) {}
@@ -210,4 +326,5 @@ class ApplicationWebGL(private val program: Program, private val configuration: 
     override var windowContentScale: Double
         get() = min(configuration.maxContentScale, window.devicePixelRatio)
         set(_) {}
+
 }
