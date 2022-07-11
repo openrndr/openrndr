077575c1ee11779979e3c54161f2262006c17bd8
vechro <vechro@vech.ro>
2022-07-11 18:13:57 +0300

    Add 4 and 8-digit support for fromHex, add tests

--- openrndr-color/src/commonTest/kotlin/org/openrndr/color/ColorRGBaTest.kt
+++ openrndr-color/src/commonTest/kotlin/org/openrndr/color/ColorRGBaTest.kt
@@ -2,7 +2,7 @@ package org.openrndr.color
 
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
-import kotlin.test.Test
+import kotlin.test.*
 
 class ColorRGBaTest {
 
@@ -28,5 +28,21 @@ class ColorRGBaTest {
             0.5
         )
         mixed.linearity shouldBe Linearity.ASSUMED_LINEAR
+    }
+
+    @Test
+    fun fromHex() {
+        assertEquals(ColorRGBa.fromHex(0x7fffff), ColorRGBa.fromHex("#7fffff"))
+        assertEquals(ColorRGBa.fromHex(0xffffff), ColorRGBa.WHITE)
+        assertEquals(ColorRGBa.fromHex(0x00ffff), ColorRGBa.CYAN)
+        assertEquals(ColorRGBa.fromHex("#f0f"), ColorRGBa.MAGENTA)
+        assertEquals(ColorRGBa.fromHex("f0f"), ColorRGBa.MAGENTA)
+        assertEquals(ColorRGBa.fromHex(0xffff00), ColorRGBa.fromHex("ff0"))
+        assertEquals(ColorRGBa.fromHex("#000001"), ColorRGBa(0.0, 0.0, 1.0 / 255.0, 1.0, Linearity.SRGB))
+        assertNotEquals(ColorRGBa.fromHex(0xffff00), ColorRGBa.fromHex("ff07"))
+        assertEquals(ColorRGBa.fromHex("0xffff0077"), ColorRGBa.fromHex("ff07"))
+        assertEquals(ColorRGBa.fromHex("ffff0077"), ColorRGBa.fromHex("#ff07"))
+        assertEquals(ColorRGBa.fromHex("0xffAA0077"), ColorRGBa.fromHex("fA07"))
+        assertEquals(ColorRGBa.fromHex("0xffff0077"), ColorRGBa(1.0, 1.0, 0.0, 119.0 / 255.0, Linearity.SRGB))
     }
 }
