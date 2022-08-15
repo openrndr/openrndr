55c0f2fab9ddd8a05e1f7754463a4de2a4aa39fa
vechro <vechro@vech.ro>
2022-08-16 00:02:08 +0300

    Variants proof of concept for openal

--- openrndr-jvm/openrndr-openal/build.gradle.kts
+++ openrndr-jvm/openrndr-openal/build.gradle.kts
@@ -2,10 +2,29 @@ plugins {
     org.openrndr.convention.`kotlin-multiplatform`
 }
 
+data class JvmNativeVariant(val targetName: String, val os: String, val arch: String)
+
+val jvmNativeVariants = listOf(
+    JvmNativeVariant("natives-linux-arm64", OperatingSystemFamily.LINUX, "arm64"),
+    JvmNativeVariant("natives-linux-x86_64", OperatingSystemFamily.LINUX, "x86-64"),
+    JvmNativeVariant("natives-macos-arm64", OperatingSystemFamily.MACOS, "arm64"),
+    JvmNativeVariant("natives-macos", OperatingSystemFamily.MACOS, "x86-64"),
+    JvmNativeVariant("natives-windows", OperatingSystemFamily.WINDOWS, "x86-64")
+)
+
 kotlin {
-    jvm {
-        testRuns["test"].executionTask.configure {
-            exclude("**/*.class")
+    for ((targetName, os, arch) in jvmNativeVariants) {
+        jvm(targetName) {
+            attributes.attribute(OperatingSystemFamily.OPERATING_SYSTEM_ATTRIBUTE, objects.named(os))
+            attributes.attribute(MachineArchitecture.ARCHITECTURE_ATTRIBUTE, objects.named(arch))
+        }
+        configurations[targetName + "ApiElements"].attributes {
+            attribute(OperatingSystemFamily.OPERATING_SYSTEM_ATTRIBUTE, objects.named(os))
+            attribute(MachineArchitecture.ARCHITECTURE_ATTRIBUTE, objects.named(arch))
+        }
+        configurations[targetName + "RuntimeElements"].attributes {
+            attribute(OperatingSystemFamily.OPERATING_SYSTEM_ATTRIBUTE, objects.named(os))
+            attribute(MachineArchitecture.ARCHITECTURE_ATTRIBUTE, objects.named(arch))
         }
     }
     sourceSets {
@@ -14,6 +33,13 @@ kotlin {
             dependencies {
                 implementation(project(":openrndr-math"))
                 implementation(libs.bundles.lwjgl.openal)
+            }
+        }
+        for ((targetName, _, _) in jvmNativeVariants) {
+            getByName(targetName + "Main") {
+                dependencies {
+                    runtimeOnly(libs.bundles.lwjgl.openal)
+                }
             }
         }
     }
