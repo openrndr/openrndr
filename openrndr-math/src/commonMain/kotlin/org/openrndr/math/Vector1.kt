eb941a48c24d360fb96f9b4d2e686769155477d2
Edwin Jakobs <edwin@rndr.studio>
2022-08-24 22:54:12 +0200

    [openrndr-math] Add Vector1

---
+++ openrndr-math/src/commonMain/kotlin/org/openrndr/math/Vector1.kt
@@ -0,0 +1,48 @@
+package org.openrndr.math
+
+import kotlin.jvm.JvmInline
+import kotlin.math.abs
+
+@JvmInline
+value class Vector1(val x: Double) : EuclideanVector<Vector1> {
+    override val zero: Vector1
+        get() = ZERO
+    override val length: Double
+        get() = abs(x)
+    override val squaredLength: Double
+        get() = x*x
+
+    override fun times(scale: Double): Vector1 {
+        return Vector1(x * scale)
+    }
+
+    override fun div(scale: Double): Vector1 {
+        return Vector1(x / scale)
+    }
+
+    override fun minus(right: Vector1): Vector1 {
+        return Vector1(x - right.x)
+    }
+
+    override fun plus(right: Vector1): Vector1 {
+        return Vector1(x + right.x)
+    }
+
+    override fun dot(right: Vector1): Double {
+        return x * right.x
+    }
+
+    override fun squaredDistanceTo(other: Vector1): Double {
+        val dx = x - other.x
+        return dx * dx
+    }
+
+    override fun distanceTo(other: Vector1): Double {
+        return abs(x - other.x)
+    }
+
+    companion object {
+        val ZERO = Vector1(0.0)
+    }
+
+}
