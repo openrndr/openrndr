a9436144c66b616cbb381a5d635232875e29d5ba
Edwin Jakobs <edwin@rndr.studio>
2023-03-22 17:20:53 +0100

    [openrndr-gl3] Fix AbstractApplicationTestFixture

--- openrndr-jvm/openrndr-gl3/src/jvmTest/kotlin/AbstractApplicationTestFixture.kt
+++ openrndr-jvm/openrndr-gl3/src/jvmTest/kotlin/AbstractApplicationTestFixture.kt
@@ -2,17 +2,20 @@ import kotlinx.coroutines.runBlocking
 import org.openrndr.Configuration
 import org.openrndr.Program
 import org.openrndr.ProgramImplementation
+import org.openrndr.internal.gl3.ApplicationBaseGLFWGL3
 import org.openrndr.internal.gl3.ApplicationGLFWGL3
 import kotlin.test.*
 
 abstract class AbstractApplicationTestFixture {
+    lateinit var applicationBase: ApplicationBaseGLFWGL3
     lateinit var application: ApplicationGLFWGL3
     lateinit var program: Program
 
     @BeforeTest
     open fun setup() {
         program = ProgramImplementation()
-        application = ApplicationGLFWGL3(program, Configuration())
+        applicationBase = ApplicationBaseGLFWGL3()
+        application = applicationBase.build(program, Configuration()) as ApplicationGLFWGL3
         runBlocking { application.setup() }
         application.preloop()
     }
