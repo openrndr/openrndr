e44e9088520833dd6b8a9d3146a46fe65b301df6
Edwin Jakobs <edwin@rndr.studio>
2023-03-30 17:50:43 +0200

    Move publishing logic to conventions, use gradle nexus publish

--- buildSrc/src/main/kotlin/org/openrndr/convention/kotlin-jvm.gradle.kts
+++ buildSrc/src/main/kotlin/org/openrndr/convention/kotlin-jvm.gradle.kts
@@ -6,11 +6,15 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val libs = the<LibrariesForLibs>()
 
+val shouldPublish = project.name !in setOf("openrndr-demos")
+
 plugins {
     java
     kotlin("jvm")
+    `maven-publish` apply false
     id("maven-publish")
     id("org.openrndr.convention.dokka")
+    signing
 }
 
 repositories {
@@ -49,5 +53,51 @@ tasks {
     withType<KotlinCompile>() {
         kotlinOptions.apiVersion = libs.versions.kotlinApi.get()
         kotlinOptions.languageVersion = libs.versions.kotlinLanguage.get()
+    }
+}
+
+val isReleaseVersion = !(version.toString()).endsWith("SNAPSHOT")
+
+if (shouldPublish) {
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                from(components["java"])
+                groupId = "org.openrndr"
+                artifactId = project.name
+                description = project.name
+                pom {
+                    name.set("$project.name")
+                    description.set("$project.name")
+                    url.set("https://openrndr.org")
+                    developers {
+                        developer {
+                            id.set("edwinjakobs")
+                            name.set("Edwin Jakobs")
+                            email.set("edwin@openrndr.org")
+                        }
+                    }
+
+                    licenses {
+                        license {
+                            name.set("BSD-2-Clause")
+                            url.set("https://github.com/openrndr/openrndr/blob/master/LICENSE")
+                            distribution.set("repo")
+                        }
+                    }
+
+                    scm {
+                        connection.set("scm:git:git@github.com:openrndr/openrndr.git")
+                        developerConnection.set("scm:git:ssh://github.com/openrndr/openrndr.git")
+                        url.set("https://github.com/openrndr/openrndr")
+                    }
+                }
+            }
+        }
+    }
+
+    signing {
+        setRequired({ isReleaseVersion && gradle.taskGraph.hasTask("publish") })
+        sign(publishing.publications)
     }
 }
--- buildSrc/src/main/kotlin/org/openrndr/convention/kotlin-multiplatform.gradle.kts
+++ buildSrc/src/main/kotlin/org/openrndr/convention/kotlin-multiplatform.gradle.kts
@@ -5,11 +5,17 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val libs = the<LibrariesForLibs>()
+val shouldPublish = project.name !in setOf("openrndr-demos")
 
 plugins {
     kotlin("multiplatform")
+    `maven-publish` apply false
     id("maven-publish")
     id("org.openrndr.convention.dokka")
+    signing
+}
+if (shouldPublish) {
+    apply(plugin = "maven-publish")
 }
 
 repositories {
@@ -62,5 +68,59 @@ kotlin {
                 runtimeOnly(libs.bundles.jupiter)
             }
         }
+    }
+}
+
+val isReleaseVersion = !(version.toString()).endsWith("SNAPSHOT")
+if (shouldPublish) {
+    publishing {
+        publications {
+            val fjdj = tasks.create("fakeJavaDocJar", Jar::class) {
+                archiveClassifier.set("javadoc")
+            }
+            matching { it.name == "jvm" }.forEach { p ->
+                p as MavenPublication
+                p.artifact(fjdj)
+            }
+            all {
+                this as MavenPublication
+                versionMapping {
+                    allVariants {
+                        fromResolutionOf("allSourceSetsRuntimeDependenciesMetadata")
+                    }
+                }
+                pom {
+                    name.set("$project.name")
+                    description.set("$project.name")
+                    url.set("https://openrndr.org")
+                    developers {
+                        developer {
+                            id.set("edwinjakobs")
+                            name.set("Edwin Jakobs")
+                            email.set("edwin@openrndr.org")
+                        }
+                    }
+
+                    licenses {
+                        license {
+                            name.set("BSD-2-Clause")
+                            url.set("https://github.com/openrndr/openrndr/blob/master/LICENSE")
+                            distribution.set("repo")
+                        }
+                    }
+
+                    scm {
+                        connection.set("scm:git:git@github.com:openrndr/orx.git")
+                        developerConnection.set("scm:git:ssh://github.com/openrndr/openrndr.git")
+                        url.set("https://github.com/openrndr/openrndr")
+                    }
+                }
+            }
+        }
+    }
+
+    signing {
+        setRequired({ isReleaseVersion && gradle.taskGraph.hasTask("publish") })
+        sign(publishing.publications)
     }
 }
