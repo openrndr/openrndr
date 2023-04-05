b69b9c77496a858faf3681040f8fc280fba71cf6
Abe Pazos <abe@hamoid.com>
2023-04-05 16:07:35 +0200

    Ensure triangulate() produces triangles with the same winding

--- openrndr-ktessellation/src/jvmTest/kotlin/TestKTessellation.kt
+++ openrndr-ktessellation/src/jvmTest/kotlin/TestKTessellation.kt
@@ -15,6 +15,28 @@ class TestKTessellation {
     fun testRectangle() {
         val c = Rectangle(300.0, 300.0, 100.0, 100.0).shape
         val tris = triangulate(c)
-        assertEquals(6, tris.size)
+        assertEquals(
+            6, tris.size,
+            "triangulate() on a rectangle should produce 6 vertices"
+        )
     }
+
+    @Test
+    fun testWinding() {
+        val c = Circle(300.0, 300.0, 100.0).shape
+        val tris = triangulate(c)
+
+        val windings = mutableSetOf<Boolean>()
+        for (i in tris.indices step 3) {
+            val a = tris[i]
+            val b = tris[i + 1]
+            val c = tris[i + 2]
+            windings.add((b.x - a.x) * (c.y - a.y) < (b.y - a.y) * (c.x - a.x))
+        }
+        assertEquals(
+            1, windings.size,
+            "triangulate() should produce triangles with only 1 type of winding"
+        )
+    }
+
 }
