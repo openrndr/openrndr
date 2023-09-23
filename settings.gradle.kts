d08f84d47e90803468244489cd46642fdaef90d1
Edwin Jakobs <edwin@rndr.studio>
2023-09-23 08:59:20 +0200

    Upgrade foojay-resolver-convention to 0.7.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "openrndr"
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.6.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
 }
 
 include(
