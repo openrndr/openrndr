9ed85a25c5a2ff6dde5ab955eae0fabe9146e200
Edwin Jakobs <edwin@rndr.studio>
2024-02-10 08:34:51 +0100

    [openrndr-gl3] Inline checkGLErrors and debugGLErrors functions

--- openrndr-jvm/openrndr-gl3/src/jvmMain/kotlin/org/openrndr/internal/gl3/ErrorGL3.kt
+++ openrndr-jvm/openrndr-gl3/src/jvmMain/kotlin/org/openrndr/internal/gl3/ErrorGL3.kt
@@ -4,25 +4,25 @@ import org.lwjgl.opengl.GL33C.*
 
 class GL3Exception(message: String) : Exception(message)
 
-fun checkGLErrors(errorFunction: ((Int)->String?)?=null) {
+inline fun checkGLErrors(crossinline errorFunction: ((Int) -> String?) = { null }) {
     val error = glGetError()
     if (error != GL_NO_ERROR) {
         val message = when (error) {
-            GL_INVALID_OPERATION             -> "GL_INVALID_OPERATION"
-            GL_INVALID_VALUE                 -> "GL_INVALID_VALUE"
-            GL_INVALID_ENUM                  -> "GL_INVALID_ENUM"
+            GL_INVALID_OPERATION -> "GL_INVALID_OPERATION"
+            GL_INVALID_VALUE -> "GL_INVALID_VALUE"
+            GL_INVALID_ENUM -> "GL_INVALID_ENUM"
             GL_INVALID_FRAMEBUFFER_OPERATION -> "GL_INVALID_FRAMEBUFFER_OPERATION"
-            GL_OUT_OF_MEMORY                 -> "GL_OUT_OF_MEMORY"
-            GL_STACK_UNDERFLOW               -> "GL_STACK_UNDERFLOW"
-            GL_STACK_OVERFLOW                -> "GL_STACK_OVERFLOW"
-            else                             -> "<untranslated: $error>"
+            GL_OUT_OF_MEMORY -> "GL_OUT_OF_MEMORY"
+            GL_STACK_UNDERFLOW -> "GL_STACK_UNDERFLOW"
+            GL_STACK_OVERFLOW -> "GL_STACK_OVERFLOW"
+            else -> "<untranslated: $error>"
         }
-        throw GL3Exception("GL ERROR: $message ${errorFunction?.invoke(error)}" )
+        throw GL3Exception("GL ERROR: $message ${errorFunction.invoke(error)}")
     }
 }
 
-fun debugGLErrors(errorFunction: ((Int)->String?)?=null) {
-    if (useDebugContext) {
+inline fun debugGLErrors(crossinline errorFunction: ((Int) -> String?) = { null }) {
+    if (DriverGL3Configuration.useDebugContext) {
         checkGLErrors(errorFunction)
     }
 }
