df8b31c3be8e5387829f40e48891068733fbda00
Edwin Jakobs <edwin@rndr.studio>
2024-03-10 14:57:52 +0100

    [openrndr-gl3] Check if shader type is supported

--- openrndr-jvm/openrndr-gl3/src/jvmMain/kotlin/org/openrndr/internal/gl3/GeometryShaderGL3.kt
+++ openrndr-jvm/openrndr-gl3/src/jvmMain/kotlin/org/openrndr/internal/gl3/GeometryShaderGL3.kt
@@ -2,10 +2,14 @@ package org.openrndr.internal.gl3
 
 import org.lwjgl.opengl.GL33C
 import org.lwjgl.opengl.GL40C
+import org.openrndr.internal.Driver
 
 class GeometryShaderGL3(val shaderObject: Int, val name: String) {
     companion object {
         fun fromString(code: String, name: String): GeometryShaderGL3 {
+            require(Driver.glType == DriverTypeGL.GL) {
+                "Geometry shaders are not supported by ${Driver.glType}"
+            }
             val shaderObject = glCreateShader(GL33C.GL_GEOMETRY_SHADER)
             glShaderSource(shaderObject, code)
             glCompileShader(shaderObject)
@@ -13,7 +17,7 @@ class GeometryShaderGL3(val shaderObject: Int, val name: String) {
             glGetShaderiv(shaderObject, GL33C.GL_COMPILE_STATUS, compileStatus)
             if (compileStatus[0] != GL33C.GL_TRUE) {
                 checkShaderInfoLog(shaderObject, code, name)
-                throw Exception("could not compile vertex shader")
+                throw Exception("could not compile geometry shader")
             }
             return GeometryShaderGL3(shaderObject, name)
         }
@@ -23,14 +27,18 @@ class GeometryShaderGL3(val shaderObject: Int, val name: String) {
 class TessellationControlShaderGL3(val shaderObject: Int, val name: String) {
     companion object {
         fun fromString(code: String, name: String): TessellationControlShaderGL3 {
-            val shaderObject = GL33C.glCreateShader(GL40C.GL_TESS_CONTROL_SHADER)
+            require(Driver.glType == DriverTypeGL.GL) {
+                "Tessellation control shaders are not supported by ${Driver.glType}"
+            }
+
+            val shaderObject = glCreateShader(GL40C.GL_TESS_CONTROL_SHADER)
             glShaderSource(shaderObject, code)
             glCompileShader(shaderObject)
             val compileStatus = IntArray(1)
             glGetShaderiv(shaderObject, GL33C.GL_COMPILE_STATUS, compileStatus)
             if (compileStatus[0] != GL33C.GL_TRUE) {
                 checkShaderInfoLog(shaderObject, code, name)
-                throw Exception("could not compile vertex shader")
+                throw Exception("could not compile tessellation control shader")
             }
             return TessellationControlShaderGL3(shaderObject, name)
         }
@@ -40,6 +48,10 @@ class TessellationControlShaderGL3(val shaderObject: Int, val name: String) {
 class TessellationEvaluationShaderGL3(val shaderObject: Int, val name: String) {
     companion object {
         fun fromString(code: String, name: String): TessellationEvaluationShaderGL3 {
+            require(Driver.glType == DriverTypeGL.GL) {
+                "Tessellation evaluation shaders are not supported by ${Driver.glType}"
+            }
+
             val shaderObject = glCreateShader(GL40C.GL_TESS_EVALUATION_SHADER)
             glShaderSource(shaderObject, code)
             glCompileShader(shaderObject)
@@ -47,7 +59,7 @@ class TessellationEvaluationShaderGL3(val shaderObject: Int, val name: String) {
             glGetShaderiv(shaderObject, GL33C.GL_COMPILE_STATUS, compileStatus)
             if (compileStatus[0] != GL33C.GL_TRUE) {
                 checkShaderInfoLog(shaderObject, code, name)
-                throw Exception("could not compile vertex shader")
+                throw Exception("could not compile tessellation evaluation shader")
             }
             return TessellationEvaluationShaderGL3(shaderObject, name)
         }
