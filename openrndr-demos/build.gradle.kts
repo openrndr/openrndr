3e2d72826b46fdf9e1c234fbd7d80a77b2b1bf44
Edwin Jakobs <edwin@rndr.studio>
2024-05-02 11:37:21 +0200

    [openrndr-demos] Fix library configuration for linux-arm64

--- openrndr-demos/build.gradle.kts
+++ openrndr-demos/build.gradle.kts
@@ -4,7 +4,11 @@ plugins {
 
 val openrndrOS = when(org.gradle.internal.os.OperatingSystem.current()) {
     org.gradle.internal.os.OperatingSystem.WINDOWS -> "windows"
-    org.gradle.internal.os.OperatingSystem.LINUX -> "linux-x64"
+    org.gradle.internal.os.OperatingSystem.LINUX -> when (System.getProperty("os.arch")) {
+        "x86-64", "x86_64", "amd64", "x64" -> "linux-x64"
+        "aarch64", "arm-v8" -> "linux-arm64"
+        else -> error("arch not supported")
+    }
     org.gradle.internal.os.OperatingSystem.MAC_OS -> {
         when (System.getProperty("os.arch")) {
             "x86-64", "x86_64", "amd64", "x64" -> "macos"
