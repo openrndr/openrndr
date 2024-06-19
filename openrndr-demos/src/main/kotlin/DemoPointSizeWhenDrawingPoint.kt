25ec75e62252ae8145bc5501234483920dc23685
Kazik Pogoda <morisil@xemantic.com>
2024-06-19 23:10:42 +0200

    improvement: code nesting in DemoPointSize* simplified

--- openrndr-demos/src/main/kotlin/DemoPointSizeWhenDrawingPoint.kt
+++ openrndr-demos/src/main/kotlin/DemoPointSizeWhenDrawingPoint.kt
@@ -3,27 +3,25 @@ import org.openrndr.draw.shadeStyle
 import org.openrndr.math.Vector2
 import kotlin.math.sin
 
-fun main() {
-    application {
-        program {
-            val style = shadeStyle {
-                vertexTransform = """
-                    x_pointSize = p_pointSize;
-                """.trimIndent()
-                fragmentTransform = """
-                    // TODO in the future it should be rather c_boundsPosition instead of gl_PointCoord
-                    vec2 pointPosition = (gl_PointCoord - .5) * 2.0;
-                    float distance = length(pointPosition);
-                    float luma = smoothstep(1.0, .7, distance);
-                    luma *= va_pointSize / 100.0;
-                    x_fill.rgb = vec3(luma);
-                """.trimIndent()
-            }
-            extend {
-                style.parameter("pointSize", (sin(seconds) * .5 + .5) * 99.0 + 1.0)
-                drawer.shadeStyle = style
-                drawer.point(Vector2(width / 2.0, height / 2.0))
-            }
+fun main() = application {
+    program {
+        val style = shadeStyle {
+            vertexTransform = """
+                x_pointSize = p_pointSize;
+            """.trimIndent()
+            fragmentTransform = """
+                // TODO in the future it should be rather c_boundsPosition instead of gl_PointCoord
+                vec2 pointPosition = (gl_PointCoord - .5) * 2.0;
+                float distance = length(pointPosition);
+                float luma = smoothstep(1.0, .7, distance);
+                luma *= va_pointSize / 100.0;
+                x_fill.rgb = vec3(luma);
+            """.trimIndent()
+        }
+        extend {
+            style.parameter("pointSize", (sin(seconds) * .5 + .5) * 99.0 + 1.0)
+            drawer.shadeStyle = style
+            drawer.point(Vector2(width / 2.0, height / 2.0))
         }
     }
 }
