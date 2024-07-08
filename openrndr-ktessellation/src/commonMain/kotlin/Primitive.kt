cf5437e5b38fa28b598a2472b50b418ded931d10
Edwin Jakobs <edwin@rndr.studio>
2024-07-08 12:48:09 +0200

    [openrndr-ktessellation] Add IndexedTessellator

--- openrndr-ktessellation/src/commonMain/kotlin/Primitive.kt
+++ openrndr-ktessellation/src/commonMain/kotlin/Primitive.kt
@@ -2,7 +2,7 @@ package org.openrndr.ktessellation
 
 import org.openrndr.math.Vector2
 
-data class Primitive(
+class Primitive(
     /**
      * The OpenGL constant defining the type of this primitive
      *
@@ -14,4 +14,18 @@ data class Primitive(
      *
      */
     val positions: MutableList<Vector2> = mutableListOf()
+}
+
+class IndexedPrimitive(
+    /**
+     * The OpenGL constant defining the type of this primitive
+     *
+     */
+    val type: Int
+) {
+    /**
+     * A list of the indices of the vertices required to draw this primitive.
+     *
+     */
+    val indices: MutableList<Int> =  mutableListOf()
 }
--- openrndr-ktessellation/src/commonMain/kotlin/Tessellator.kt
+++ openrndr-ktessellation/src/commonMain/kotlin/Tessellator.kt
@@ -6,7 +6,6 @@ import org.openrndr.math.Vector2
 class Tessellator : GLUtessellatorImpl() {
     var primitives = mutableListOf<Primitive>()
 
-
     init {
         val callback: GLUtessellatorCallbackAdapter =
             object : GLUtessellatorCallbackAdapter() {
@@ -45,3 +44,50 @@ class Tessellator : GLUtessellatorImpl() {
         gluTessCallback(GLU.GLU_TESS_COMBINE, callback)
     }
 }
+
+
+class IndexedTessellator : GLUtessellatorImpl() {
+    var primitives = mutableListOf<IndexedPrimitive>()
+
+    init {
+        val callback: GLUtessellatorCallbackAdapter =
+            object : GLUtessellatorCallbackAdapter() {
+                override fun begin(type: Int) {
+                    primitives.add(IndexedPrimitive(type))
+                }
+
+                override fun vertexData(vertexData: Any?, polygonData: Any?) {
+                    val data = vertexData as Int
+                    primitives[primitives.size - 1].indices.add(data)
+                }
+
+                override fun error(errnum: Int) {
+                    throw RuntimeException("GLU Error " + gluErrorString(errnum))
+                }
+
+
+                override fun combine(
+                    coords: DoubleArray?,
+                    data: Array<Any?>?,
+                    weight: FloatArray?,
+                    outData: Array<Any?>?
+                ) {
+
+                    coords ?: error("coords == null")
+                    outData ?: error("outData == null")
+                    val result = DoubleArray(3)
+                    result[0] = coords[0]
+                    result[1] = coords[1]
+                    result[2] = coords[2]
+                    data!!
+
+                    outData[0] = data[0]
+                }
+            }
+        gluTessCallback(GLU.GLU_TESS_BEGIN, callback)
+        gluTessCallback(GLU.GLU_TESS_VERTEX_DATA, callback)
+        gluTessCallback(GLU.GLU_TESS_ERROR, callback)
+        gluTessCallback(GLU.GLU_TESS_COMBINE, callback)
+    }
+}
+
