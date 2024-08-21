e64efdc8ffd8bab58f9f4810ea0aa245d88e8fd3
Edwin Jakobs <edwin@rndr.studio>
2024-08-21 18:00:57 +0200

    [openrndr-gl3] Add angle backend for macos-x64

--- openrndr-jvm/openrndr-gl3/src/jvmMain/kotlin/org/openrndr/internal/gl3/angle/Angle.kt
+++ openrndr-jvm/openrndr-gl3/src/jvmMain/kotlin/org/openrndr/internal/gl3/angle/Angle.kt
@@ -2,15 +2,26 @@ package org.openrndr.internal.gl3.angle
 
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.openrndr.internal.gl3.DriverGL3Configuration
+import org.openrndr.platform.Platform
+import org.openrndr.platform.PlatformArchitecture
 import java.io.File
 
 private class Angle
 
 private val logger = KotlinLogging.logger { }
 fun extractAngleLibraries() {
+
+    val platform = "macos"
+    val arch = when (Platform.architecture) {
+        PlatformArchitecture.X86_64 -> "x64"
+        PlatformArchitecture.AARCH64 -> "arm64"
+        else -> error("architecture not supported")
+    }
+
+
     logger.info { "Loading ANGLE libraries from resources" }
     val targetDirectory = File(".")// Platform.tempDirectory()
-    val egl = Angle::class.java.getResourceAsStream("/org/openrndr/internal/gl3/angle/libEGL.dylib")
+    val egl = Angle::class.java.getResourceAsStream("/org/openrndr/internal/gl3/angle/$platform/$arch/libEGL.dylib")
         ?: error("libEGL.dylib not found in resources")
     val targetEgl = File(targetDirectory, "libEGL.dylib")
 
@@ -23,7 +34,7 @@ fun extractAngleLibraries() {
 
     val targetGlesv2 = File(targetDirectory, "libGLESv2.dylib")
     if (!targetGlesv2.exists() || DriverGL3Configuration.overwriteExistingAngle) {
-        val glesv2 = Angle::class.java.getResourceAsStream("/org/openrndr/internal/gl3/angle/libGLESv2.dylib") ?: error(
+        val glesv2 = Angle::class.java.getResourceAsStream("/org/openrndr/internal/gl3/angle/$platform/$arch/libGLESv2.dylib") ?: error(
             "libGLESv2.dylib not found in resources"
         )
         targetGlesv2.outputStream().use {
