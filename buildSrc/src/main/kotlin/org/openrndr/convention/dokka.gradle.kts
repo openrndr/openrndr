1d7b81cc036d9e6dc7804a0b3a62b1a180a156eb
Abe Pazos <abe@hamoid.com>
2024-12-06 16:49:09 +0100

    Dokka: fix: only worked if build/dokka existed

--- buildSrc/src/main/kotlin/org/openrndr/convention/dokka.gradle.kts
+++ buildSrc/src/main/kotlin/org/openrndr/convention/dokka.gradle.kts
@@ -2,7 +2,7 @@ package org.openrndr.convention
 
 import org.jetbrains.dokka.gradle.DokkaMultiModuleTask
 import org.jetbrains.dokka.gradle.DokkaTaskPartial
-import java.net.URL
+import java.net.URI
 
 plugins {
     id("org.jetbrains.dokka")
@@ -15,20 +15,24 @@ repositories {
 tasks.withType<DokkaTaskPartial> {
     dokkaSourceSets.configureEach {
         skipDeprecated.set(true)
+
         val sourcesDirectory = try {
             file("src/$name/kotlin", PathValidation.EXISTS)
         } catch (_: InvalidUserDataException) {
             return@configureEach
         }
+
         // Specifies the location of the project source code on the Web.
         // If provided, Dokka generates "source" links for each declaration.
         sourceLink {
             // Unix based directory relative path to the root of the project (where you execute gradle respectively).
-            localDirectory.set(sourcesDirectory)
+            localDirectory = sourcesDirectory
+
             // URL showing where the source code can be accessed through the web browser
-            remoteUrl.set(
-                URL("https://github.com/openrndr/openrndr/blob/master/${moduleName.get()}/src/$name/kotlin")
-            )
+            remoteUrl = URI(
+                "https://github.com/openrndr/openrndr/blob/master/${moduleName.get()}/src/$name/kotlin"
+            ).toURL()
+
             // Suffix which is used to append the line number to the URL. Use #L for GitHub
             remoteLineSuffix.set("#L")
         }
@@ -38,16 +42,19 @@ tasks.withType<DokkaTaskPartial> {
 // Since pluginConfiguration doesn't seem to work, manual customization here
 tasks.withType<DokkaMultiModuleTask> {
     doLast {
-        val customCSS = file("dokka/styles/extra.css").readText()
-        val cssPath = file("build/dokka/htmlMultiModule/styles/style.css")
-        val defaultCSS = cssPath.readText()
-        if(!defaultCSS.contains(customCSS)) {
-            cssPath.writeText(defaultCSS + customCSS)
-        }
+        // Runs for JS, JVM and root. Only for the last one build/dokka/ exists.
+        val cssPath = rootDir.resolve("build/dokka/htmlMultiModule/styles/style.css")
+        if(cssPath.exists()) {
+            val defaultCSS = cssPath.readText()
+            val customCSS = rootDir.resolve("dokka/styles/extra.css").readText()
+            if(!defaultCSS.contains(customCSS)) {
+                cssPath.writeText(defaultCSS + customCSS)
+            }
 
-        copy {
-            from("dokka/images/logo-icon.svg")
-            into("build/dokka/htmlMultiModule/images/")
+            copy {
+                from("dokka/images/logo-icon.svg")
+                into("build/dokka/htmlMultiModule/images/")
+            }
         }
     }
 }
