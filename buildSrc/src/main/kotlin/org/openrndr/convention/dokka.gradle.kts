b209737de490eb98491c049e9999037f119ebe8a
Abe Pazos <abe@hamoid.com>
2024-12-21 10:16:35 +0100

    Update dokka to 2.0.0 (#419)

--- buildSrc/src/main/kotlin/org/openrndr/convention/dokka.gradle.kts
+++ buildSrc/src/main/kotlin/org/openrndr/convention/dokka.gradle.kts
@@ -1,8 +1,5 @@
 package org.openrndr.convention
 
-import org.jetbrains.dokka.gradle.DokkaTaskPartial
-import java.net.URL
-
 plugins {
     id("org.jetbrains.dokka")
 }
@@ -11,27 +8,31 @@ repositories {
     mavenCentral()
 }
 
-tasks.withType<DokkaTaskPartial> {
-    dokkaSourceSets {
-        configureEach {
-            skipDeprecated.set(true)
-            val sourcesDirectory = try {
-                file("src/$name/kotlin", PathValidation.EXISTS)
-            } catch (_: InvalidUserDataException) {
-                return@configureEach
-            }
-            // Specifies the location of the project source code on the Web.
-            // If provided, Dokka generates "source" links for each declaration.
-            sourceLink {
-                // Unix based directory relative path to the root of the project (where you execute gradle respectively).
-                localDirectory.set(sourcesDirectory)
-                // URL showing where the source code can be accessed through the web browser
-                remoteUrl.set(
-                    URL("https://github.com/openrndr/openrndr/blob/master/${moduleName.get()}/src/$name/kotlin")
-                )
-                // Suffix which is used to append the line number to the URL. Use #L for GitHub
-                remoteLineSuffix.set("#L")
-            }
+dokka {
+    pluginsConfiguration.html {
+        customStyleSheets.from("dokka/styles/extra.css")
+        customAssets.from("dokka/images/logo-icon.svg")
+    }
+    dokkaSourceSets.configureEach {
+        skipDeprecated.set(true)
+
+        val sourcesDirectory = try {
+            file("src/$name/kotlin", PathValidation.EXISTS)
+        } catch (_: InvalidUserDataException) {
+            return@configureEach
+        }
+
+        // Specifies the location of the project source code on the Web.
+        // If provided, Dokka generates "source" links for each declaration.
+        sourceLink {
+            // Unix based directory relative path to the root of the project (where you execute gradle respectively).
+            localDirectory = sourcesDirectory
+
+            // URL showing where the source code can be accessed through the web browser
+            remoteUrl("https://github.com/openrndr/openrndr/blob/master/${moduleName.get()}/src/$name/kotlin")
+
+            // Suffix which is used to append the line number to the URL. Use #L for GitHub
+            remoteLineSuffix.set("#L")
         }
     }
 }
