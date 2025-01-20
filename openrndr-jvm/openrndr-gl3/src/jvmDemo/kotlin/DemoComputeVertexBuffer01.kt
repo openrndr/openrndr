f316d6d4341ddaf1527aa1122e60197003e72c46
Edwin Jakobs <edwin@rndr.studio>
2025-01-20 20:28:24 +0100

    [openrndr-gl3] Update DemoComputeVertexBuffer01.kt

--- openrndr-jvm/openrndr-gl3/src/jvmDemo/kotlin/DemoComputeVertexBuffer01.kt
+++ openrndr-jvm/openrndr-gl3/src/jvmDemo/kotlin/DemoComputeVertexBuffer01.kt
@@ -2,16 +2,23 @@ import org.openrndr.application
 import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.*
 import org.openrndr.math.IntVector3
-import java.nio.ByteBuffer
-import java.nio.ByteOrder
 
+/**
+ * This program demonstrates the use of vertex buffers, compute shaders, and rendering techniques
+ * in a creative and dynamic way.
+ *
+ * Main operations include:
+ * - The creation of two vertex buffers (`vb0` and `vb1`) to hold vertex data.
+ * - Population of the `vb0` vertex buffer with initial random vertex positions.
+ * - Creation of shader storage buffer views (`ssb0` and `ssb1`) from the vertex buffers.
+ * - Establishment of a compute style to perform vertex transformation via compute shaders.
+ * - Execution of the compute style which updates vertex positions with animated transformations.
+ * - Drawing of the transformed vertex buffer using triangular primitives with partial opacity applied.
+ *
+ */
 fun main() {
     application {
         program {
-
-            val ib = indexBuffer(99, IndexType.INT32)
-            val ssib = ib.shaderStorageBufferView()
-
             val vb0 = vertexBuffer(vertexFormat {
                 position(3)
                 paddingFloat(1)
@@ -37,24 +44,16 @@ fun main() {
                     computeTransform = """
                         int idx = int(gl_GlobalInvocationID.x);
                         b_output_vertices.vertex[idx] = b_input_vertices.vertex[idx];
-                        b_output_vertices.vertex[idx].position.x += cos(b_output_vertices.vertex[idx].position.y*0.02) * 40.0;
-                        b_indices.indices[idx] = idx;
+                        b_output_vertices.vertex[idx].position.x += cos(p_time + b_input_vertices.vertex[idx].position.y*0.02) * 40.0;
+                        b_output_vertices.vertex[idx].position.y += sin(p_time + b_input_vertices.vertex[idx].position.x*0.02) * 40.0;
             """.trimIndent()
                     buffer("input_vertices", ssb0)
                     buffer("output_vertices", ssb1)
-                    buffer("indices", ssib)
+                    parameter("time", seconds)
                 }
                 cs.execute(99, 1, 1)
 
-                val bb = ByteBuffer.allocateDirect(99 * 4)
-                bb.order(ByteOrder.nativeOrder())
-                ib.read(bb)
-                bb.rewind()
-
-                drawer.fill = ColorRGBa.RED
-                drawer.vertexBuffer(vb0, DrawPrimitive.TRIANGLES)
-
-                drawer.fill = ColorRGBa.BLUE
+                drawer.fill = ColorRGBa.WHITE.opacify(0.25)
                 drawer.vertexBuffer(vb1, DrawPrimitive.TRIANGLES)
             }
         }
