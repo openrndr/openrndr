e335405ebffe5aa036a2d2bfd6914219def1e044
Edwin Jakobs <edwin@rndr.studio>
2025-06-26 12:11:00 +0200

    [openrndr-gl3] Fix GLES support in DrawThreadGL3

--- openrndr-jvm/openrndr-gl3/src/jvmMain/kotlin/org/openrndr/internal/gl3/DrawThreadGL3.kt
+++ openrndr-jvm/openrndr-gl3/src/jvmMain/kotlin/org/openrndr/internal/gl3/DrawThreadGL3.kt
@@ -2,8 +2,17 @@ package org.openrndr.internal.gl3
 
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.lwjgl.glfw.GLFW
+import org.lwjgl.glfw.GLFW.GLFW_CLIENT_API
+import org.lwjgl.glfw.GLFW.GLFW_CONTEXT_CREATION_API
+import org.lwjgl.glfw.GLFW.GLFW_EGL_CONTEXT_API
+import org.lwjgl.glfw.GLFW.GLFW_OPENGL_CORE_PROFILE
+import org.lwjgl.glfw.GLFW.GLFW_OPENGL_ES_API
+import org.lwjgl.glfw.GLFW.GLFW_OPENGL_FORWARD_COMPAT
+import org.lwjgl.glfw.GLFW.GLFW_OPENGL_PROFILE
+import org.lwjgl.glfw.GLFW.glfwWindowHint
 import org.lwjgl.opengl.GL
-import org.lwjgl.opengl.GL33C.*
+import org.lwjgl.opengl.GL43C
+import org.lwjgl.opengles.GLES
 import org.lwjgl.system.MemoryUtil
 import org.openrndr.Dispatcher
 import org.openrndr.draw.DrawThread
@@ -20,16 +29,25 @@ class DrawThreadGL3(private val contextWindow: Long) : DrawThread {
             GLFW.glfwDefaultWindowHints()
             val version = (Driver.instance as DriverGL3).version
             logger.debug { "creating new GL context (version ${version.majorVersion}.${version.minorVersion}" }
-            GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, version.majorVersion)
-            GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, version.minorVersion)
-            GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE)
-            GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_PROFILE, GLFW.GLFW_OPENGL_CORE_PROFILE)
-            GLFW.glfwWindowHint(GLFW.GLFW_RED_BITS, 8)
-            GLFW.glfwWindowHint(GLFW.GLFW_GREEN_BITS, 8)
-            GLFW.glfwWindowHint(GLFW.GLFW_BLUE_BITS, 8)
-            GLFW.glfwWindowHint(GLFW.GLFW_STENCIL_BITS, 8)
-            GLFW.glfwWindowHint(GLFW.GLFW_DEPTH_BITS, 24)
-            GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GLFW.GLFW_FALSE)
+            glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, version.majorVersion)
+            glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, version.minorVersion)
+            when (Driver.glType) {
+                DriverTypeGL.GL -> {
+                    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL43C.GL_TRUE)
+                    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE)
+                }
+
+                DriverTypeGL.GLES -> {
+                    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API)
+                    glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_EGL_CONTEXT_API)
+                }
+            }
+            glfwWindowHint(GLFW.GLFW_RED_BITS, 8)
+            glfwWindowHint(GLFW.GLFW_GREEN_BITS, 8)
+            glfwWindowHint(GLFW.GLFW_BLUE_BITS, 8)
+            glfwWindowHint(GLFW.GLFW_STENCIL_BITS, 8)
+            glfwWindowHint(GLFW.GLFW_DEPTH_BITS, 24)
+            glfwWindowHint(GLFW.GLFW_VISIBLE, GLFW.GLFW_FALSE)
             val contextWindow = GLFW.glfwCreateWindow(1,
                     1,
                     "", MemoryUtil.NULL, primaryWindow)
@@ -52,7 +70,12 @@ class DrawThreadGL3(private val contextWindow: Long) : DrawThread {
         thread(isDaemon = true, name = "DrawThreadGL3") {
             realDispatcher = Dispatcher()
             GLFW.glfwMakeContextCurrent(contextWindow)
-            GL.createCapabilities()
+
+            when (Driver.glType) {
+                DriverTypeGL.GL -> GL.createCapabilities()
+                DriverTypeGL.GLES -> GLES.createCapabilities()
+            }
+
             realDrawer = Drawer(Driver.instance)
             val vaos = IntArray(1)
             glGenVertexArrays(vaos)
