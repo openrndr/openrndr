431c2228241b340a26276e650523ae6611df600b
Abe Pazos <abe@hamoid.com>
2025-07-04 14:06:56 +0200

    Replace some kluent tests with kotlin.test

--- openrndr-core/src/test/kotlin/TestTriangle.kt
+++ openrndr-core/src/test/kotlin/TestTriangle.kt
@@ -1,20 +1,27 @@
-import org.amshove.kluent.`should be equal to`
 import org.openrndr.math.Vector2
 import org.openrndr.shape.Triangle
-import io.kotest.core.spec.style.DescribeSpec
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
-class TestTriangle : DescribeSpec({
-    describe("a triangle") {
+class TestTriangle {
+    @Test
+    fun `a triangle should contain inner points`() {
         val t = Triangle(Vector2(0.0, 0.0), Vector2(0.0, 100.0), Vector2(100.0, 100.0))
-        it("contains points") {
-            (Vector2(30.0, 30.0) in t) `should be equal to` true
-            (Vector2(1.0, 90.0) in t) `should be equal to` true
-        }
-
-        it("does not contain other points") {
-            (Vector2(31.1, 30.0) in t) `should be equal to` false
-            (Vector2(40.0, 30.0) in t) `should be equal to` false
-            (Vector2(200.0, 200.0) in t) `should be equal to` false
-        }
+        assertTrue(Vector2(1.0, 90.0) in t, "should contain inner point")
     }
-})
+
+    @Test
+    fun `a triangle should contain edge points`() {
+        val t = Triangle(Vector2(0.0, 0.0), Vector2(0.0, 100.0), Vector2(100.0, 100.0))
+        assertTrue(Vector2(30.0, 30.0) in t, "should contain edge points")
+    }
+
+    @Test
+    fun `a triangle should not contain outer points`() {
+        val t = Triangle(Vector2(0.0, 0.0), Vector2(0.0, 100.0), Vector2(100.0, 100.0))
+        assertFalse(Vector2(31.1, 30.0) in t, "should not contain outer points")
+        assertFalse(Vector2(40.0, 30.0) in t, "should not contain outer points")
+        assertFalse(Vector2(200.0, 200.0) in t, "should not contain outer points")
+    }
+}
