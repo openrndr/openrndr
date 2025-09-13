8f621d75a9f5fa81758479887b9682b757fef8f7
Edwin Jakobs <edwin@rndr.studio>
2025-09-13 13:58:51 +0200

    Remove cruft

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -1,18 +1,7 @@
-import org.gradle.accessors.dm.LibrariesForLibs
 plugins {
     `kotlin-dsl`
-    //id("org.openrndr.convention.variant")
 }
 
 repositories {
     gradlePluginPortal()
 }
-
-
-//gradlePlugin {
-//    // Define the plugin
-//    val attributeVariants by plugins.creating {
-//        id = "org.openrndr.convention.av"
-//        implementationClass = "org.openrndr.convention.AttributeVariants"
-//    }
-//}
