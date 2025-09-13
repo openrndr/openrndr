70e7b47ca45b9cea152f54359390a0959257f20a
Edwin Jakobs <edwin@rndr.studio>
2025-09-13 14:31:34 +0200

    Remove left-over catalog configuration in root build.gradle, fix publications

--- openrndr-dependency-catalog/build.gradle.kts
+++ openrndr-dependency-catalog/build.gradle.kts
@@ -15,7 +15,36 @@ group = "org.openrndr"
 publishing {
     publications {
         create<MavenPublication>("maven") {
+            groupId = "org.openrndr"
+            artifactId = project.name
+            description = project.name
             from(components["versionCatalog"])
+            pom {
+                name.set(project.name)
+                description.set(project.name)
+                url.set("https://openrndr.org")
+                developers {
+                    developer {
+                        id.set("edwinjakobs")
+                        name.set("Edwin Jakobs")
+                        email.set("edwin@openrndr.org")
+                    }
+                }
+
+                licenses {
+                    license {
+                        name.set("BSD-2-Clause")
+                        url.set("https://github.com/openrndr/openrndr/blob/master/LICENSE")
+                        distribution.set("repo")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git@github.com:openrndr/openrndr.git")
+                    developerConnection.set("scm:git:ssh://github.com/openrndr/openrndr.git")
+                    url.set("https://github.com/openrndr/openrndr")
+                }
+            }
         }
     }
 }
