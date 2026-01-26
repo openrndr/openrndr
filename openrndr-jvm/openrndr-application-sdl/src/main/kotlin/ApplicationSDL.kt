f5c82e951ff48b4759fb5844d47f8fd79f113fed
Abe Pazos <abe@hamoid.com>
2026-01-26 19:00:23 +0100

    Redundant semicolor

--- openrndr-jvm/openrndr-application-sdl/src/main/kotlin/ApplicationSDL.kt
+++ openrndr-jvm/openrndr-application-sdl/src/main/kotlin/ApplicationSDL.kt
@@ -111,12 +111,12 @@ class ApplicationSDL(override var program: Program, override var configuration: 
 
     private fun createPrimaryWindow() {
         SDL_GL_ResetAttributes()
-        SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-        SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
-        SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
-        SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
-        SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
-        SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+        SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1)
+        SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1)
+        SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8)
+        SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8)
+        SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8)
+        SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8)
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE)
 
         if (DriverGL3Configuration.driverType == DriverTypeGL.GLES) {
