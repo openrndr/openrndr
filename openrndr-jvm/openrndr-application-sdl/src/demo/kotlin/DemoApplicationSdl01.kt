a64f28b7a5d7f16d18f0dc2693c672f5af4d4137
Abe Pazos <abe@hamoid.com>
2026-03-25 11:47:23 +0100

    [openrndr-application-sdl] Use dropTexts in DemoApplicationSdl01.kt

--- openrndr-jvm/openrndr-application-sdl/src/demo/kotlin/DemoApplicationSdl01.kt
+++ openrndr-jvm/openrndr-application-sdl/src/demo/kotlin/DemoApplicationSdl01.kt
@@ -5,7 +5,13 @@ fun main() {
 
         program {
             window.drop.listen {
+                println("files")
                 println(it.files)
+            }
+
+            window.dropTexts.listen {
+                println("texts")
+                println(it.texts)
             }
 
             extend {
