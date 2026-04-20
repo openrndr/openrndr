121c49182b2fd7dcb97a880b226abeab2babeab5
aBe <abe@hamoid.com>
2026-04-20 14:24:53 +0200

    ApplicationSDL: add dropTexts event (#462)

--- openrndr-jvm/openrndr-application-sdl/src/demo/kotlin/DemoApplicationSdl01.kt
+++ openrndr-jvm/openrndr-application-sdl/src/demo/kotlin/DemoApplicationSdl01.kt
@@ -24,7 +24,13 @@ fun main() {
             }
 
             window.drop.listen {
+                println("files")
                 println(it.files)
+            }
+
+            window.dropTexts.listen {
+                println("texts")
+                println(it.texts)
             }
 
             extend {
